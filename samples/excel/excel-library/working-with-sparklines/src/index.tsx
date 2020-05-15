3cf24e08557c225c3a506c5ef261bb4c2ee71354
HUSSAR-mtrela <martin.trela@gmail.com>
2020-05-15 12:28:48 -0400

    updad teIndex

--- samples/excel/excel-library/working-with-sparklines/src/index.tsx
+++ samples/excel/excel-library/working-with-sparklines/src/index.tsx
@@ -1,7 +1,9 @@
+/* {RepositoryWarning}  */
+/* {RepositoryUrl}/tree/master/sample-template-files/src/index  */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './sandbox.config.json'; // required for code sandbox
 import './index.css'; // styles shared between all samples
 
 import ExcelLibrarySparklines from './ExcelLibrarySparklines';
