2e059245ad698bb1d720122b641a9f6f7e979d09
Michael DiFilippo <mdifilippo@infragistics.com>
2020-07-24 12:43:13 -0400

    mdd-add-cell-editing-sample
    mdd-add-cell-editing-sample

---
+++ samples/grids/data-grid/cell-editing/src/index.tsx
@@ -0,0 +1,12 @@
+
+
+/* {RepositoryWarning}  */
+/* {RepositoryUrl}/tree/master/templates/sample/src/index  */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css'; // styles shared between all samples
+
+import DataGridCellEditing from './DataGridCellEditing';
+ReactDOM.render(<DataGridCellEditing />, document.getElementById('root'));
