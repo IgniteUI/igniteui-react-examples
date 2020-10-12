5a08665fc3696e2b428fdabc3de457b72c36e1cf
agoldenbaum <agoldenbaum@infragistics.com>
2020-10-12 17:21:38 -0400

    Check in a new sample for grid live data without the interface

---
+++ samples/grids/data-grid/type-marketing-table/src/TaskUtil.ts
@@ -0,0 +1,23 @@
+
+
+export class TaskUtil {
+
+    public static tasks: any = {};
+
+    public static start(taskName: string) {
+        this.tasks[taskName] = new Date();
+    }
+
+    public static stop(taskName: string): number {
+        const currTime = new Date();
+        if (this.tasks[taskName] === undefined) {
+            this.tasks[taskName] = currTime;
+            return 0;
+        } else {
+            const startTime = this.tasks[taskName] as Date;
+            const duration = currTime.getTime() - startTime.getTime();
+            console.log("Task '" + taskName + "' finished in " + (duration / 1000.0) + "s");
+            return duration;
+        }
+    }
+}
