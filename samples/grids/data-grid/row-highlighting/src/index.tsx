0f0a4a2a1ab423e932e366d5157d66d94fb43a48
Michael DiFilippo <mdifilippo@infragistics.com>
2020-11-16 17:57:00 -0500

    mdd-update-to-20.2 && Row Highlighting
    mdd-update-to-20.2 && Row Highlighting

---
+++ samples/grids/data-grid/row-highlighting/src/index.tsx
@@ -0,0 +1,12 @@
+
+
+/* {RepositoryWarning}  */
+/* {RepositoryUrl}/tree/master/templates/sample/src/index  */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css'; // styles shared between all samples
+
+import DataGridRowHighlighting from './DataGridRowHighlighting';
+ReactDOM.render(<DataGridRowHighlighting />, document.getElementById('root'));
