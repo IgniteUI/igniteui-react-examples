453f401fd2992b894de0b7176b394d3218708515
Michael DiFilippo <mdifilippo@infragistics.com>
2020-12-01 14:36:13 -0500

    mdd-20.2-combo
    mdd-20.2-combo

---
+++ samples/editors/multi-column-combobox/overview/src/MultiColumnComboBoxOverview.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { SampleComboData } from './SampleComboData';
+import { IgrMultiColumnComboBoxModule } from 'igniteui-react-grids';
+import { IgrMultiColumnComboBox, SortMode } from 'igniteui-react-grids';
+
+IgrMultiColumnComboBoxModule.register();
+
+export default class IgrMultiColumnComboBoxOverview extends React.Component<any, any> {
+    public multiColumnComboBox!: IgrMultiColumnComboBox;
+    public countryData: any[];
+    
+    constructor(props: any) {
+        super(props);
+        this.onMultiColumnComboBoxRef = this.onMultiColumnComboBoxRef.bind(this);
+        this.countryData = SampleComboData.getPopulation();
+    }
+
+    public onMultiColumnComboBoxRef(multiColumnComboBox: IgrMultiColumnComboBox) {
+        if (!multiColumnComboBox) { return; }
+        this.multiColumnComboBox = multiColumnComboBox;
+        this.multiColumnComboBox.dataSource = this.countryData;
+        this.multiColumnComboBox.textField = "country";
+        this.multiColumnComboBox.sortMode = SortMode.SortByOneColumnOnly;
+    }
+
+    public render(): JSX.Element {
+        return (
+            <div className="igContainer">    
+                <div className="igComponent">               
+                    <IgrMultiColumnComboBox width="400px"
+                            ref={this.onMultiColumnComboBoxRef}>
+                    </IgrMultiColumnComboBox>
+                </div>
+            </div>
+        );
+    }
+}
