b5a18e51fe04fd153bc29c5729a5fd87b4ec24e2
HUSSAR-mtrela <martin.trela@gmail.com>
2021-05-11 14:47:44 -0400

    fixed geomap sample

--- samples/maps/geo-map/navigation/src/index.tsx
+++ samples/maps/geo-map/navigation/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { MapUtils, MapRegion } from './MapUtils';
 // for handling of maps events
-import { IgrRectChangedEventArgs } from 'igniteui-react-core';
+import { IgrRectChangedEventArgs, IgRect } from 'igniteui-react-core';
 import { IgrDataChartInteractivityModule, IgrSeriesViewer } from 'igniteui-react-charts';
 import { IgrGeographicMapModule } from 'igniteui-react-maps';
 import { IgrGeographicMap } from 'igniteui-react-maps';
@@ -45,10 +45,10 @@ export default class MapNavigation extends React.Component<any, any> {
             mapHoverRelativeCoordinate: { x: 0, y: 0 },
             mapHoverWindowCoordinate: { x: 0, y: 0 },
 
-            windowRect: { left: 0, top: 0, height: 1, width: 1 },
             windowScale: 0,
             windowPositionVertical: 0,
             windowPositionHorizontal: 0,
+            getRect: { left: -180, top: -75, height: 170, width: 360 },
             geoT: "",
             geoL: "",
             geoH: "",
@@ -68,7 +68,6 @@ export default class MapNavigation extends React.Component<any, any> {
                     <IgrGeographicMap
                         ref={this.onMapRef}
                         actualWindowRectChanged={this.onMapWindowRectChanged}
-
                         width="100%"
                         height="100%"
                         zoomable="true"/>
@@ -76,32 +75,30 @@ export default class MapNavigation extends React.Component<any, any> {
 
                 <div className="overlay-left overlay-border vertical" style={{background: "rgba(217, 217, 217, 0.5)"}} >
                     <label style={{fontWeight: 600}}>Zoom to Map Region</label>
-                    <select value={this.state.selectedRegion}
-                            onChange={this.onSelectionChanged}>
+                    <select value={this.state.selectedRegion} onChange={this.onSelectionChanged}>
                             {this.NavigationOptions}
                     </select>
                     <label style={{fontWeight: 600}}>Map Geographic Rect</label>
                     <div className="horizontal" >
                         <div className="vertical"  style={{marginRight: "1rem"}}>
-                            <label >{geoT}</label>
-                            <label >{geoL}</label>
+                            <label >{this.state.geoT}</label>
+                            <label >{this.state.geoL}</label>
                         </div>
                         <div className="vertical">
-                            <label >{geoH}</label>
-                            <label >{geoW}</label>
+                            <label >{this.state.geoH}</label>
+                            <label >{this.state.geoW}</label>
                         </div>
                     </div>
 
                     <label style={{fontWeight: 600}}>Map Window Rect</label>
-
                     <div className="horizontal" >
                         <div className="vertical"  style={{marginRight: "1rem"}}>
-                            <label >{winT}</label>
-                            <label >{winL}</label>
+                            <label >{this.state.winT}</label>
+                            <label >{this.state.winL}</label>
                         </div>
                         <div className="vertical">
-                            <label >{winH}</label>
-                            <label >{winW}</label>
+                            <label >{this.state.winH}</label>
+                            <label >{this.state.winW}</label>
                         </div>
                     </div>
 
@@ -122,23 +119,22 @@ export default class MapNavigation extends React.Component<any, any> {
                     <label style={{fontWeight: 600}}>Map Hover Coordinates</label>
                     <div className="horizontal">
                         <div className="vertical" style={{marginRight: "1rem"}}>
-                            <label >Longitude: </label>
-                            <label >Latitude: </label>
                             <label >Window X: </label>
                             <label >Window Y: </label>
+                            <label >Longitude: </label>
+                            <label >Latitude: </label>
                             <label >Pixel X: </label>
                             <label >Pixel Y: </label>
                         </div>
                         <div className="vertical">
-                            <label >{MapUtils.toLng(this.state.mapHoverGeographicCoordinate.x)}</label>
-                            <label >{MapUtils.toLat(this.state.mapHoverGeographicCoordinate.y)}</label>
                             <label >{this.state.mapHoverWindowCoordinate.x.toFixed(4)}</label>
                             <label >{this.state.mapHoverWindowCoordinate.y.toFixed(4)}</label>
+                            <label >{MapUtils.toLng(this.state.mapHoverGeographicCoordinate.x)}</label>
+                            <label >{MapUtils.toLat(this.state.mapHoverGeographicCoordinate.y)}</label>
                             <label >{MapUtils.toPixel(this.state.mapHoverRelativeCoordinate.x)}</label>
                             <label >{MapUtils.toPixel(this.state.mapHoverRelativeCoordinate.y)}</label>
                         </div>
                     </div>
-
                 </div>
 
                 <div className="overlay-bottom-right overlay-border">Imagery Tiles: @ESRI/ArcGIS</div>
@@ -164,7 +160,7 @@ export default class MapNavigation extends React.Component<any, any> {
         this.geoMap = geoMap;
         this.geoMap.zoomToGeographic({ left:-134.5, top:16.5, width:70.0, height:37.0 });
 
-        // optional
+        // optional imagery background that provides context for map regions with oceans
         const tileSource = new IgrArcGISOnlineMapImagery();
         tileSource.mapServerUri = "https://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer";
         this.geoMap.backgroundContent = tileSource;
@@ -176,50 +172,32 @@ export default class MapNavigation extends React.Component<any, any> {
 
     public onMapWindowRectChanged(viewer: IgrSeriesViewer, e: IgrRectChangedEventArgs) {
         let geoMap = viewer as IgrGeographicMap;
+
+        // storing window location and size (values between 0.0 and 1.0)
+        const windowRect: IgRect = e.newRect;
+
         // converting window rect to geographic rect/region:
-        const geoRect: any = geoMap.getGeographicFromZoom(e.newRect);
-        geoRect.bottom = geoRect.top + geoRect.height;
-        geoRect.right = geoRect.left + geoRect.width;
+        const geoRect: any = geoMap.getGeographicFromZoom(windowRect);
+        geoRect.bottom = geoRect.top  + geoRect.height;
+        geoRect.right  = geoRect.left + geoRect.width;
         // calculating center of geographic region
+        geoRect.latitude  = geoRect.top  + (geoRect.height / 2);
         geoRect.longitude = geoRect.left + (geoRect.width / 2);
-        geoRect.latitude = geoRect.top + (geoRect.height / 2);
-
-        const h = geoMap.actualWindowPositionHorizontal;
-        const v = geoMap.actualWindowPositionVertical;
-        const s = geoMap.actualWindowScale;
-
-        const geoT = "T: " + MapUtils.toLat(this.geoRect.top);
-        const geoL = "L: " + MapUtils.toLng(this.geoRect.left);
-        const geoH = "H: " + MapUtils.toLng(this.geoRect.height);
-        const geoW = "W: " + MapUtils.toLng(this.geoRect.width);
-
-        const winT = "T: " + windowRect.top.toFixed(4);
-        const winL = "L: " + windowRect.left.toFixed(4);
-        const winH = "H: " + windowRect.height.toFixed(4);
-        const winW = "W: " + windowRect.width.toFixed(4);
 
         this.setState({
             mapRegion: geoRect,
-            windowRect: e.newRect,
-            windowPositionHorizontal: h,
-            windowPositionVertical: v,
-            windowScale: s,
-            geoT: geoT,
-            geoL: geoL,
-            geoH: geoH,
-            geoW: geoW,
-            winT: winT,
-            winL: winL,
-            winH: winH,
-            winW: winW,
+            windowPositionHorizontal: geoMap.actualWindowPositionHorizontal,
+            windowPositionVertical: geoMap.actualWindowPositionVertical,
+            windowScale: geoMap.actualWindowScale,
+            geoT: "T: " + MapUtils.toLat(geoRect.top),
+            geoL: "L: " + MapUtils.toLng(geoRect.left),
+            geoH: "H: " + MapUtils.toLng(geoRect.height),
+            geoW: "W: " + MapUtils.toLng(geoRect.width),
+            winT: "T: " + windowRect.top.toFixed(4),
+            winL: "L: " + windowRect.left.toFixed(4),
+            winH: "H: " + windowRect.height.toFixed(4),
+            winW: "W: " + windowRect.width.toFixed(4),
         });
-
-        // console.log("{ " +
-        // "left: " + geoRect.left.toFixed(1) + ", " +
-        // "top: " + geoRect.top.toFixed(1) + ", " +
-        // "width: "  + geoRect.width.toFixed(1) + ", " +
-        // "height: "  + geoRect.height.toFixed(1) + " " +
-        // "}");
     }
 
     public onMapMouseMove(e: any) {
@@ -242,10 +220,6 @@ export default class MapNavigation extends React.Component<any, any> {
             mapHoverRelativeCoordinate: relativeCoordinate,
             mapHoverWindowCoordinate: windowCoordinate
         });
-
-        // console.log(
-        //     " win: " + winPoint.x.toFixed(3) + ", "  + winPoint.y.toFixed(3) + " >>" +
-        //     " geo: " + mapPoint.x.toFixed(0) + ", "  + mapPoint.y.toFixed(0));
     }
 
     public componentDidMount() {
