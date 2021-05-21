266020b3f8cd4707d7e76e502906d091b348a067
ig-robstoffers <rstoffers@infragistics.com>
2021-05-21 17:03:05 -0400

    chart sample fixes

--- samples/charts/data-chart/type-scatter-series/src/index.tsx
+++ samples/charts/data-chart/type-scatter-series/src/index.tsx
@@ -9,11 +9,6 @@ import { IgrBubbleSeries } from 'igniteui-react-charts';
 import { IgrScatterSeries } from 'igniteui-react-charts';
 import { IgrScatterLineSeries } from 'igniteui-react-charts';
 import { IgrScatterSplineSeries } from 'igniteui-react-charts';
-// elements of scatter series:
-import { IgrSizeScale } from 'igniteui-react-charts';
-import { IgrValueBrushScale } from 'igniteui-react-charts';
-import { IgrCustomPaletteBrushScale } from 'igniteui-react-charts';
-import { BrushSelectionMode } from 'igniteui-react-charts';
 // modules of data chart:
 import { IgrDataChart } from 'igniteui-react-charts';
 import { IgrDataChartCoreModule } from 'igniteui-react-charts';
@@ -35,7 +30,7 @@ IgrDataChartInteractivityModule.register();
 IgrNumberAbbreviatorModule.register();
 IgrLegendModule.register();
 
-export default class DataChartTypeScatterMarkerSeries extends React.Component<any, any> {
+export default class DataChartTypeScatterSeries extends React.Component<any, any> {
     public data: any[];
     public chart: IgrDataChart;
     public legend: IgrLegend;
@@ -47,53 +42,50 @@ export default class DataChartTypeScatterMarkerSeries extends React.Component<an
         this.onLegendRef = this.onLegendRef.bind(this);
         this.onSeriesTypeChanged = this.onSeriesTypeChanged.bind(this);
 
-        this.state = { seriesType: "Bubble" }
+        this.state = { seriesType: "Point" }
     }
 
     public render(): JSX.Element {
         return (
         <div className="container sample">
-            <div className="options horizontal">
-                <label className="options-label">Type of Scatter Series: </label>
-                <select value={this.state.seriesType}
-                    onChange={this.onSeriesTypeChanged}>
-                    <option>Bubble</option>
-                    <option>Point</option>
-                    <option>Line</option>
-                    <option>Spline</option>
-                </select>
-                <label className="legend-title">Legend: </label>
-                <div className="options vertical" >
+            <div className="options vertical">
+                <span className="legend-title">Legend: </span>
+                <div className="legend" >
                     <IgrLegend ref={this.onLegendRef} orientation="Horizontal" />
                 </div>
+
+                <div className="overlay-right">
+                    <div className="options horizontal">
+                        <label className="options-label">Scatter Series: </label>
+                        <select value={this.state.seriesType} onChange={this.onSeriesTypeChanged}>
+                            <option>Bubble</option>
+                            <option>Point</option>
+                            <option>Line</option>
+                            <option>Spline</option>
+                        </select>
+                    </div>
+                </div>
             </div>
+
             <div className="container" style={{height: "calc(100% - 35px)"}} >
-                <IgrDataChart ref={this.onChartRef}
-                    isHorizontalZoomEnabled={true}
-                    isVerticalZoomEnabled={true}
+                <IgrDataChart
+                    ref={this.onChartRef}
                     width="100%"
                     height="100%"
-                    chartTitle="GDP vs Population">
+                    chartTitle="GDP vs Population"
+                    isHorizontalZoomEnabled={true}
+                    isVerticalZoomEnabled={true}>
                     {/* axes: */}
-                    <IgrNumericXAxis name="xAxis"
-                    isLogarithmic={true}
-                    abbreviateLargeNumbers={true}
-                    title="Population" />
-                    <IgrNumericYAxis name="yAxis"
-                    isLogarithmic={true}
-                    abbreviateLargeNumbers={true}
-                    title="Total GDP ($)" />
-
-                    {/* series are created in the setSeries function
-                        alternatively, you can create series using this code: */}
-                    {/* <IgrBubbleSeries
-                    name="series1"
-                    dataSource={this.data}
-                    xAxisName="xAxis"
-                    yAxisName="yAxis"
-                    xMemberPath="Population"
-                    yMemberPath="GdpTotal"
-                    radiusMemberPath="GdpPerCapita"  /> */}
+                    <IgrNumericXAxis
+                        name="xAxis"
+                        isLogarithmic={true}
+                        abbreviateLargeNumbers={true}
+                        title="Population" />
+                    <IgrNumericYAxis
+                        name="yAxis"
+                        isLogarithmic={true}
+                        abbreviateLargeNumbers={true}
+                        title="Total GDP ($)" />
                </IgrDataChart>
             </div>
         </div>
@@ -104,9 +96,9 @@ export default class DataChartTypeScatterMarkerSeries extends React.Component<an
         if (!chart) { return; }
 
         this.chart = chart;
+        this.setSeries(this.state.seriesType);
         if (this.legend) {
             this.chart.legend = this.legend;
-            this.setSeries("Bubble");
         }
     }
 
@@ -124,129 +116,103 @@ export default class DataChartTypeScatterMarkerSeries extends React.Component<an
         this.setSeries(selectedSeries);
     }
 
-    public setSeries(seriesType: string)
-    {
+    public setSeries(seriesType: string) {
         if (seriesType === "Point") {
             const series1 = new IgrScatterSeries({ name: "series1" });
-            series1.title = "Rich Countries";
             series1.markerType = MarkerType.Circle;
-            series1.dataSource = SampleScatterStats.getCountriesWithHighIncome();
             series1.showDefaultTooltip = true;
-            series1.xMemberPath = "Population";
-            series1.yMemberPath = "GdpTotal";
+            series1.title = "High Income Countries";
             series1.xAxisName = "xAxis";
             series1.yAxisName = "yAxis";
-
+            series1.xMemberPath = "Population";
+            series1.yMemberPath = "GdpTotal";
+            series1.dataSource = SampleScatterStats.getCountriesWithHighIncome();
+            
             const series2 = new IgrScatterSeries({ name: "series2" });
-            series2.title = "Poor Countries";
             series2.markerType = MarkerType.Circle;
-            series2.dataSource = SampleScatterStats.getCountriesWithLowIncome();
             series2.showDefaultTooltip = true;
-            series2.xMemberPath = "Population";
-            series2.yMemberPath = "GdpTotal";
+            series2.title = "Low Income Countries";
             series2.xAxisName = "xAxis";
             series2.yAxisName = "yAxis";
-
+            series2.xMemberPath = "Population";
+            series2.yMemberPath = "GdpTotal";
+            series2.dataSource = SampleScatterStats.getCountriesWithLowIncome();
+            
             this.chart.series.clear();
             this.chart.series.add(series1);
             this.chart.series.add(series2);
-
         } else if (seriesType === "Line") {
             const series1 = new IgrScatterLineSeries({ name: "series1" });
-            series1.title = "Rich Countries";
             series1.markerType = MarkerType.Circle;
-            series1.dataSource = SampleScatterStats.getCountriesWithHighIncome();
             series1.showDefaultTooltip = true;
-            series1.xMemberPath = "Population";
-            series1.yMemberPath = "GdpTotal";
+            series1.title = "High Income Countries";
             series1.xAxisName = "xAxis";
             series1.yAxisName = "yAxis";
+            series1.xMemberPath = "Population";
+            series1.yMemberPath = "GdpTotal";
+            series1.dataSource = SampleScatterStats.getCountriesWithHighIncome();
 
             const series2 = new IgrScatterLineSeries({ name: "series2" });
-            series2.title = "Poor Countries";
             series2.markerType = MarkerType.Circle;
-            series2.dataSource = SampleScatterStats.getCountriesWithLowIncome();
             series2.showDefaultTooltip = true;
-            series2.xMemberPath = "Population";
-            series2.yMemberPath = "GdpTotal";
+            series2.title = "Low Income Countries";
             series2.xAxisName = "xAxis";
             series2.yAxisName = "yAxis";
-
+            series2.xMemberPath = "Population";
+            series2.yMemberPath = "GdpTotal";
+            series2.dataSource = SampleScatterStats.getCountriesWithLowIncome();
+            
             this.chart.series.clear();
             this.chart.series.add(series1);
             this.chart.series.add(series2);
-
         } else if (seriesType === "Spline") {
             const series1 = new IgrScatterSplineSeries({ name: "series1" });
-            series1.title = "Rich Countries";
             series1.markerType = MarkerType.Circle;
-            series1.dataSource = SampleScatterStats.getCountriesWithHighIncome();
             series1.showDefaultTooltip = true;
-            series1.xMemberPath = "Population";
-            series1.yMemberPath = "GdpTotal";
+            series1.title = "High Income Countries";
             series1.xAxisName = "xAxis";
             series1.yAxisName = "yAxis";
-
+            series1.xMemberPath = "Population";
+            series1.yMemberPath = "GdpTotal";
+            series1.dataSource = SampleScatterStats.getCountriesWithHighIncome();
+            
             const series2 = new IgrScatterSplineSeries({ name: "series2" });
-            series2.title = "Poor Countries";
             series2.markerType = MarkerType.Circle;
-            series2.dataSource = SampleScatterStats.getCountriesWithLowIncome();
             series2.showDefaultTooltip = true;
-            series2.xMemberPath = "Population";
-            series2.yMemberPath = "GdpTotal";
+            series2.title = "Low Income Countries";
             series2.xAxisName = "xAxis";
             series2.yAxisName = "yAxis";
-
+            series2.xMemberPath = "Population";
+            series2.yMemberPath = "GdpTotal";
+            series2.dataSource = SampleScatterStats.getCountriesWithLowIncome();
+            
             this.chart.series.clear();
             this.chart.series.add(series1);
             this.chart.series.add(series2);
-
         } else if (seriesType === "Bubble") {
-            const sizeScale = new IgrSizeScale({});
-            sizeScale.minimumValue = 10;
-            sizeScale.maximumValue = 60;
-
-            const brushScale1 = new IgrValueBrushScale({});
-            brushScale1.brushes = ["#FFFFFF", "#b56ffc"];
-            brushScale1.minimumValue = 10;
-            brushScale1.maximumValue = 60;
-
             const series1 = new IgrBubbleSeries({ name: "series1" });
-            series1.title = "Large Countries";
             series1.markerType = MarkerType.Circle;
-            series1.dataSource = SampleScatterStats.getCountriesWithLargePop();
             series1.showDefaultTooltip = true;
-            series1.xMemberPath = "Population";
-            series1.yMemberPath = "GdpTotal";
-            series1.radiusMemberPath = "GdpPerCapita";
-            series1.radiusScale = sizeScale;
-            // series1.fillMemberPath = "GdpPerCapita";
-            // series1.fillScale = brushScale1;
+            series1.title = "High Income Countries";
             series1.xAxisName = "xAxis";
             series1.yAxisName = "yAxis";
-
-            const brushScale2 = new IgrCustomPaletteBrushScale({});
-            brushScale2.brushes = ["#FFFFFF", "#b56ffc"];
-            brushScale2.brushSelectionMode = BrushSelectionMode.Interpolate;
+            series1.xMemberPath = "Population";
+            series1.yMemberPath = "GdpTotal";
+            series1.dataSource = SampleScatterStats.getCountriesWithHighIncome();
 
             const series2 = new IgrBubbleSeries({ name: "series2" });
-            series2.title = "Small Countries";
             series2.markerType = MarkerType.Circle;
-            series2.dataSource = SampleScatterStats.getCountriesWithSmallPop();
             series2.showDefaultTooltip = true;
-            series2.xMemberPath = "Population";
-            series2.yMemberPath = "GdpTotal";
-            series2.radiusMemberPath = "GdpPerCapita";
-            series2.radiusScale = sizeScale;
-            // series2.fillMemberPath = "GdpPerCapita";
-            // series2.fillScale = brushScale2;
+            series2.title = "Low Income Countries";
             series2.xAxisName = "xAxis";
             series2.yAxisName = "yAxis";
+            series2.xMemberPath = "Population";
+            series2.yMemberPath = "GdpTotal";
+            series2.dataSource = SampleScatterStats.getCountriesWithLowIncome();
 
             this.chart.series.clear();
             this.chart.series.add(series1);
             this.chart.series.add(series2);
-
         }
     }
 }
