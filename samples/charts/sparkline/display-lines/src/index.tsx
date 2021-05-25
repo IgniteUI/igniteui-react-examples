d93340a157a99d402f1e8c3f006682e9aef3bff9
Michael Peterson <MPeterson@infragistics.com>
2021-05-25 14:54:42 -0400

    update sparkline data

--- samples/charts/sparkline/display-lines/src/index.tsx
+++ samples/charts/sparkline/display-lines/src/index.tsx
@@ -13,89 +13,63 @@ export default class SparklineDisplayLines extends React.Component {
 
     constructor(props: any) {
         super(props);
-        this.data = this.createData(360 * 1.5);
+        this.data = this.generateData();
     }
 
     public render(): JSX.Element {
         return (
             <div className="container sample">
-                <div className="container">
-                    <IgrSparkline height="100%" width="100%" displayType="Line"
-                        dataSource={this.data} minimum={-1} maximum={1}
-                        valueMemberPath="Revenue"/>
-                </div>
                 <label className="options-label">Revenue Sparkline</label>
                 <div className="container">
                     <IgrSparkline height="100%" width="100%" displayType="Line"
                         dataSource={this.data} minimum={-1} maximum={1}
-                        valueMemberPath="Income"/>
+                        valueMemberPath="Value" labelMemberPath="Angle"/>
                 </div>
                 <label className="options-label">Income Sparkline</label>
                 <div className="container">
                     <IgrSparkline height="100%" width="100%" displayType="Line"
                         dataSource={this.data} minimum={-1} maximum={1}
-                        valueMemberPath="Expanse" />
+                        valueMemberPath="Income" labelMemberPath="Angle"/>
                 </div>
                 <label className="options-label">Expanse Sparkline</label>
+                <div className="container">
+                    <IgrSparkline height="100%" width="100%" displayType="Line"
+                        dataSource={this.data} minimum={-1} maximum={1}
+                        valueMemberPath="Expanse" labelMemberPath="Angle"/>
+                </div>
             </div >
         );
     }
 
-    public createData(itemsCount: number): any[] {
-        const data: any[] = [];
-        let revenue = 0;
-        for (let i = 0; i <= itemsCount; i += 10)
-        {
-            const v1 = Math.sin(i * Math.PI / 180);
-            const v2 = Math.sin(3 * i * Math.PI / 180) / 3;
-            revenue = v1 + v2;
-            let expanse = revenue < 0 ? revenue : 0;
-            let income = revenue > 0 ? revenue : 0;
-            data.push({ Label: i, Income: income, Expanse: expanse, Revenue: revenue });
-        }
-        return data;
-    }
-
-    public createData1(itemsCount: number): any[] {
-        const data: any[] = [];
-        let v1 = 0;
-        let v2 = 5;
-        for (let i = 0; i <= itemsCount; i++) {
-            v1 += (Math.random() - 0.5) * 4;
-            v1 = this.clamp(v1, -10, 10);
-            v2 += (Math.random() - 0.5) * 4;
-            v2 = this.clamp(v2, -10, 10);
-            let l = i.toString();
-            data.push({ Label: l, Value1: v1, Value2: v2 });
-        }
-        return data;
-    }
-
-    public createData2(itemsCount: number): any[] {
+    public generateData()
+    {
         const data: any[] = [];
         let index = 0;
-        for (let angle = 0; angle <= itemsCount; angle += 10)
+        let min = 1000.0;
+        let max = -1000.0;
+
+        for (let angle = 0; angle < 360 * 4; angle += 5)
         {
-            const v1 = Math.sin(angle * Math.PI / 180);
-            const v2 = Math.cos(angle * Math.PI / 180);
+            let v1 = Math.sin(angle * Math.PI / 180);
+            let v2 = Math.sin(3 * angle * Math.PI / 180) / 3;
+            let revenue = v1 + v2;
+            let expanse = revenue < 0 ? revenue : 0;
+            let income = revenue > 0 ? revenue : 0;
+
             data.push({
                 "Index": index++,
                 "Angle": angle,
-                "Value1": v1,
-                "Value2": v2,
+                // Value = v1 + v2
+                "Value": revenue,
+                "Expanse": expanse,
+                "Income": income
             });
-        }
-        return data;
-    }
 
-    public clamp(v: number, min: number, max: number): number {
-        if (v > max) {
-            v = max;
+            min = Math.min(min, v1 + v2);
+            max = Math.max(max, v1 + v2);
         }
-        else if (v < min) {
-            v = min;
-        }
-        return v;
+
+        return data;
     }
 
 }
