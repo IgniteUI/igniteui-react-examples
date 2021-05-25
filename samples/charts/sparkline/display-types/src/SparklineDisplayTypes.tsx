d1d49b618e395df79603aaefdf801b0b2c197c81
Michael Peterson <MPeterson@infragistics.com>
2021-05-25 12:13:17 -0400

    sparkline data update

--- samples/charts/sparkline/display-types/src/SparklineDisplayTypes.tsx
+++ samples/charts/sparkline/display-types/src/SparklineDisplayTypes.tsx
@@ -14,56 +14,66 @@ export default class SparklineDisplayTypes extends React.Component {
     constructor(props: any) {
         super(props);
 
-        this.data = this.createData(360 * 1.5);
+        this.data = this.generateData()
     }
 
     public render(): JSX.Element {
         return (
             <div className="igContainer">
-                <label className="igOptions-label">Area Sparkline</label>
-                <div className="igComponent">
-                    <IgrSparkline height="100%" width="100%" displayType="Area"
-                        dataSource={this.data} minimum={-1} maximum={1}
-                        valueMemberPath="Value"/>
-                </div>
-                <label className="igOptions-label">Line Sparkline</label>
+                <label className="igOptions-label">Sparkline with Line Display Type</label>
                 <div className="igComponent">
                     <IgrSparkline height="100%" width="100%" displayType="Line"
                         dataSource={this.data} minimum={-1} maximum={1}
-                        valueMemberPath="Value" />
+                        valueMemberPath="Value" labelMemberPath="Angle"/>
                 </div>
-                <label className="igOptions-label">Column Sparkline</label>
+                <label className="igOptions-label">Sparkline with Area Display Type</label>
+                <div className="igComponent">
+                    <IgrSparkline height="100%" width="100%" displayType="Area"
+                        dataSource={this.data} minimum={-1} maximum={1}
+                        valueMemberPath="Value" labelMemberPath="Angle"/>
+                </div>
+                <label className="igOptions-label">Sparkline with Column Display Type</label>
                 <div className="igComponent">
                     <IgrSparkline height="100%" width="100%" displayType="Column"
                         dataSource={this.data} minimum={-1} maximum={1}
-                        valueMemberPath="Value"/>
+                        valueMemberPath="Value" labelMemberPath="Angle"/>
                 </div>
-                <label className="igOptions-label">WinLoss Sparkline</label>
+                <label className="igOptions-label">Sparkline with WinLoss Display Type</label>
                 <div className="igComponent">
                     <IgrSparkline height="100%" width="100%" displayType="WinLoss"
                         dataSource={this.data} minimum={-1} maximum={1}
-                        valueMemberPath="Value" />
+                        valueMemberPath="Value" labelMemberPath="Angle"/>
                 </div>
             </div >
         );
     }
 
-    public createData(itemsCount: number): any[] {
+    public generateData()
+    {
         const data: any[] = [];
         let index = 0;
-        let min = 1000;
-        let max =-1000
-        for (let angle = 0; angle <= itemsCount; angle += 10)
+        let min = 1000.0;
+        let max = -1000.0;
+
+        for (let angle = 0; angle < 360 * 4; angle += 5)
         {
-            const v1 = Math.sin(angle * Math.PI / 180);
-            const v2 = Math.sin(3 * angle * Math.PI / 180) / 3;
+            let v1 = Math.sin(angle * Math.PI / 180);
+            let v2 = Math.sin(3 * angle * Math.PI / 180) / 3;
+            let revenue = v1 + v2;
+            let expanse = revenue < 0 ? revenue : 0;
+            let income = revenue > 0 ? revenue : 0;
+
             data.push({
                 "Index": index++,
                 "Angle": angle,
-                "Value": v1 + v2
+                // Value = v1 + v2
+                "Value": revenue,
+                "Expanse": expanse,
+                "Income": income
             });
-            min = Math.min(min, v1+v2);
-            max = Math.max(max, v1+v2);
+
+            min = Math.min(min, v1 + v2);
+            max = Math.max(max, v1 + v2);
         }
 
         return data;
