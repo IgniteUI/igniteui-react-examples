31875076d164bb104acb5c1e29580c5568a6c8f5
Michael Peterson <MPeterson@infragistics.com>
2021-05-26 15:04:43 -0400

    final value layer styling sample

---
+++ samples/charts/data-chart/final-value-layer-styling/src/SampleCategoryData.ts
@@ -0,0 +1,20 @@
+
+
+export class SampleCategoryData {
+
+    public static create(): any[] {
+        const data: any[] = [];
+        data.push({ "Year": "2009", "Europe": 31, "China": 21, "USA": 19 });
+        data.push({ "Year": "2010", "Europe": 43, "China": 26, "USA": 24 });
+        data.push({ "Year": "2011", "Europe": 66, "China": 29, "USA": 28 });
+        data.push({ "Year": "2012", "Europe": 69, "China": 32, "USA": 26 });
+        data.push({ "Year": "2013", "Europe": 58, "China": 47, "USA": 38 });
+        data.push({ "Year": "2014", "Europe": 40, "China": 46, "USA": 31 });
+        data.push({ "Year": "2015", "Europe": 78, "China": 50, "USA": 19 });
+        data.push({ "Year": "2016", "Europe": 13, "China": 90, "USA": 52 });
+        data.push({ "Year": "2017", "Europe": 78, "China": 132, "USA": 50 });
+        data.push({ "Year": "2018", "Europe": 40, "China": 134, "USA": 34 });
+        data.push({ "Year": "2019", "Europe": 80, "China": 96, "USA": 38 });
+        return data;
+    }
+}
