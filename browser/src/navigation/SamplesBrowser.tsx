befdd6cac4916acd30dae8c33d7dbabb8d47a794
HUSSAR-mtrela <martin.trela@gmail.com>
2021-06-03 12:31:51 -0400

    included all samples

--- browser/src/navigation/SamplesBrowser.tsx
+++ browser/src/navigation/SamplesBrowser.tsx
@@ -13,12 +13,12 @@ import { RoutingSample } from './SamplesData';
 import { RoutingGroup } from './SamplesData';
 
 import { gaugesRoutingData } from "../samples/gauges/RoutingData";
-// import { gridsRoutingData } from "../samples/grids/RoutingData";
+import { gridsRoutingData } from "../samples/grids/RoutingData";
 import { chartsRoutingData } from "../samples/charts/RoutingData";
-// import { mapsRoutingData } from "../samples/maps/RoutingData";
-// import { excelRoutingData } from "../samples/excel/RoutingData";
-//import { layoutsRoutingData } from "../samples/layouts/RoutingData";
-// import { editorsRoutingData } from "../samples/editors/RoutingData";
+import { mapsRoutingData } from "../samples/maps/RoutingData";
+import { excelRoutingData } from "../samples/excel/RoutingData";
+import { layoutsRoutingData } from "../samples/layouts/RoutingData";
+import { editorsRoutingData } from "../samples/editors/RoutingData";
 
 // https://material-ui.com/components/material-icons/
 import IconButton from '@material-ui/core/IconButton';
@@ -66,13 +66,13 @@ export class SamplesBrowser extends React.Component<any, any>
         // console.log(TestsRoutes.DataRoutes)
 
         const routingProviders: RoutingGroup[] = [
-            // layoutsRoutingData,
-            // gridsRoutingData,
+            layoutsRoutingData,
+            gridsRoutingData,
             chartsRoutingData,
-            // mapsRoutingData,
+            mapsRoutingData,
             gaugesRoutingData,
-            // excelRoutingData,
-            // editorsRoutingData
+            excelRoutingData,
+            editorsRoutingData
         ];
 
         for (const routingData of routingProviders) {
