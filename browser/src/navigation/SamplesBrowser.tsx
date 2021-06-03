c810973960360b1f1c12a1e4ec3e729f1313bc52
HUSSAR-mtrela <martin.trela@gmail.com>
2021-06-03 11:10:36 -0400

    fixed lint warning

--- browser/src/navigation/SamplesBrowser.tsx
+++ browser/src/navigation/SamplesBrowser.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 
 import * as React from "react";
-import { Route, Switch, Link } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import { withRouter } from 'react-router-dom';
 
 import './Scrollbars.css';
