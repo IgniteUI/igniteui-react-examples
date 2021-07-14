1cfa912a89e03b5bbd09507457222c27071b16fd
MikeD <mddifilippo89@comcast.net>
2021-07-14 18:19:33 -0400

    mdd-fix-lint-errors
    post 21.1

--- samples/charts/category-chart/axis-range/src/index.tsx
+++ samples/charts/category-chart/axis-range/src/index.tsx
@@ -115,7 +115,7 @@ export default class Sample extends React.Component<any, any> {
     public get renderer(): ComponentRenderer {
         if (this._componentRenderer == null) {
             this._componentRenderer = new ComponentRenderer();
-            var context = this._componentRenderer.context;
+            let context = this._componentRenderer.context;
             PropertyEditorDescriptionModule.register(context);
             LegendDescriptionModule.register(context);
             CategoryChartDescriptionModule.register(context);
