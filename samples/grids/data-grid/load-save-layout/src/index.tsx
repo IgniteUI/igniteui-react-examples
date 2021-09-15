af54a47b9c02875025df7f83cf81dc8e6813eb85
Michael DiFilippo <mdifilippo@infragistics.com>
2021-09-15 17:48:06 -0400

    Update index.tsx
    mdd-slim-down

--- samples/grids/data-grid/load-save-layout/src/index.tsx
+++ samples/grids/data-grid/load-save-layout/src/index.tsx
@@ -12,21 +12,14 @@ import { IgrTextColumn } from 'igniteui-react-grids';
 import { IgrNumericColumn } from 'igniteui-react-grids';
 import { IgrDateTimeColumn } from 'igniteui-react-grids';
 import { IgrImageColumn } from 'igniteui-react-grids';
-import { IgrTemplateColumn } from 'igniteui-react-grids';
-import { IgrTemplateCellUpdatingEventArgs } from 'igniteui-react-grids';
-import { IgrTemplateCellInfo } from 'igniteui-react-grids';
-import { IIgrCellTemplateProps } from 'igniteui-react-grids';
 import { IgrColumnGroupDescription } from 'igniteui-react-grids';
 import { IgrColumnSummaryDescription } from 'igniteui-react-grids'
 import { SummaryOperand } from 'igniteui-react-core';
-import { IgrSparkline } from 'igniteui-react-charts';
-import { IgrSparklineModule } from 'igniteui-react-charts';
 import Button from '@material-ui/core/Button';
 
 IgrDataGridModule.register();
 IgrDataGridToolbarModule.register();
 IgrGridColumnOptionsModule.register();
-IgrSparklineModule.register();
 
 export default class DataGridLoadSaveLayout extends React.Component<any, any> {
 
@@ -37,10 +30,7 @@ export default class DataGridLoadSaveLayout extends React.Component<any, any> {
 
     constructor(props: any) {
         super(props);
-
-        this.onAddressCellUpdating = this.onAddressCellUpdating.bind(this);
-        this.onSalesCellUpdating = this.onSalesCellUpdating.bind(this);
-        this.onEmailCellUpdating = this.onEmailCellUpdating.bind(this);
+        
         this.data = DataGridSharedData.getEmployees(100);
 
         this.onGridRef = this.onGridRef.bind(this);
@@ -48,6 +38,16 @@ export default class DataGridLoadSaveLayout extends React.Component<any, any> {
 
         this.onLoadLayoutClicked = this.onLoadLayoutClicked.bind(this);
         this.onSaveLayoutClicked = this.onSaveLayoutClicked.bind(this);
+    }
+
+    public onLoadLayoutClicked() {
+        this.grid.loadLayout(this.savedLayout);
+    }
+
+    public onSaveLayoutClicked() {
+        
+        this.grid.saveLayout();
+        this.savedLayout = this.grid.saveLayout();
     }
 
     public render(): JSX.Element {
@@ -100,13 +100,8 @@ export default class DataGridLoadSaveLayout extends React.Component<any, any> {
                     horizontalAlignment="stretch" width="110" paddingTop="5" paddingBottom="5"  paddingRight="10"/>
                     <IgrTextColumn field="Name" width="*>140"/>
 
-                    <IgrTemplateColumn field="Sales" headerText="Sales" horizontalAlignment="center"
-                    paddingLeft="10" paddingRight="0"
-                        cellUpdating={this.onSalesCellUpdating} width="*>160" />
-
-                    <IgrTemplateColumn field="Productivity" headerText="Productivity"
-                    horizontalAlignment="center" width="*>150"
-                    template={this.getProductivityChart} />
+                    <IgrNumericColumn field="Sales" headerText="Sales" horizontalAlignment="center"
+                    paddingLeft="10" paddingRight="0" width="*>160" />
 
                     <IgrNumericColumn field="Salary" positivePrefix="$"
                     showGroupingSeparator="true" width="*>160"/>
@@ -116,16 +111,10 @@ export default class DataGridLoadSaveLayout extends React.Component<any, any> {
                     <IgrImageColumn field="CountryFlag" headerText="Country" contentOpacity="1"
                     horizontalAlignment="stretch" width="130" paddingTop="5" paddingBottom="5" />
 
-                    <IgrTemplateColumn field="Address" headerText="Address" horizontalAlignment="left"
-                        cellUpdating={this.onAddressCellUpdating} width="*>160" />
+                    <IgrTextColumn field="Address" headerText="Address" horizontalAlignment="left" width="*>160" />
 
-                    <IgrTemplateColumn field="Phone" horizontalAlignment="center"
-                        cellUpdating={this.onPhoneCellUpdating} width="140" />
+                    <IgrTextColumn field="Phone" horizontalAlignment="center" width="140" />
 
-                    {/* <IgrTemplateColumn field="Email" horizontalAlignment="center"
-                    cellUpdating={this.onEmailCellUpdating} width="160" /> */}
-
-                    {/* <IgrTextColumn field="Country" width="*>140" horizontalAlignment="center"/> */}
                     <IgrTextColumn field="Income" width="*>130" horizontalAlignment="center"/>
                     <IgrTextColumn field="Age" width="*>110" horizontalAlignment="center"/>
                </IgrDataGrid>
@@ -181,196 +170,7 @@ export default class DataGridLoadSaveLayout extends React.Component<any, any> {
         this.grid.summaryDescriptions.add(salary);
     }
 
-    public getProductivityChart(props: IIgrCellTemplateProps) {
-        const info = props.dataContext as IgrTemplateCellInfo;
-        return (
-            <div className="container">
-               <IgrSparkline
-                   height="30px" width="100%"
-                   displayType="Column"
-                   dataSource={info.rowItem.Productivity}
-                   valueMemberPath="Value"
-                   labelMemberPath="Week"
-                   lineThickness={2}
-                   brush="rgb(21, 190, 6)"
-                   negativeBrush="rgb(211, 17, 3)" />
-            </div>
-        );
-    }
-
-    public onAddressCellUpdating(s: IgrTemplateColumn, e: IgrTemplateCellUpdatingEventArgs) {
-        const content = e.content as HTMLDivElement;
-        let span1: HTMLSpanElement | null = null;
-        let span2: HTMLSpanElement | null = null;
-        const info = e.cellInfo as IgrTemplateCellInfo;
-        const item = info.rowItem;
-
-        if (content.childElementCount === 0) {
-
-            span1 = document.createElement("span");
-            span2 = document.createElement("span");
-
-            content.style.fontFamily = "Verdana";
-            content.style.fontSize = "13px";
-            content.style.verticalAlign = "center";
-            content.style.lineHeight = "normal";
-            content.style.display = "flex";
-            content.style.flexDirection = "column";
-            content.style.justifyContent = "center";
-            content.style.height = "100%";
-            content.style.width = "100%";
-            content.appendChild(span1);
-            content.appendChild(span2);
-        }
-        else {
-            span1 = content.children[0] as HTMLSpanElement;
-            span2 = content.children[1] as HTMLSpanElement;
-        }
-
-        if (span1 && span2) {
-            span1.textContent = item.Street;
-            span2.textContent = item.City + ", " + item.Country;
-        }
-    }
-
-    public onSalesCellUpdating(s: IgrTemplateColumn, e: IgrTemplateCellUpdatingEventArgs) {
-        const content = e.content as HTMLDivElement;
-        const info = e.cellInfo as IgrTemplateCellInfo;
-        const sales = info.rowItem.Sales;
-        let gaugeValue: HTMLSpanElement | null = null;
-        let gaugeBar: HTMLDivElement | null = null;
-
-        if (content.childElementCount === 0) {
-            gaugeValue = document.createElement("span");
-            gaugeValue.style.margin = "0px";
-            gaugeValue.style.marginTop = "2px";
-            gaugeValue.style.padding = "0px";
-            gaugeValue.style.fontFamily = "Verdana";
-            gaugeValue.style.fontSize = "13px";
-            gaugeValue.style.textAlign = "center";
-
-            gaugeBar = document.createElement("div");
-            gaugeBar.style.background = "#7f7f7f";
-            gaugeBar.style.padding = "0px";
-            gaugeBar.style.margin = "0px";
-            gaugeBar.style.height = "6px";
-            gaugeBar.style.left = "0px";
-            gaugeBar.style.top = "0px";
-            gaugeBar.style.position = "relative";
-
-            const gauge = document.createElement("div");
-            gauge.style.background = "#dddddd";
-            gauge.style.padding = "0px";
-            gauge.style.margin = "0px";
-            gauge.style.height = "6px";
-            gauge.style.marginTop = "8px";
-            gauge.style.width = "100%";
-            gauge.appendChild(gaugeBar);
-
-            content.style.verticalAlign = "center";
-            content.style.lineHeight = "normal";
-            content.style.display = "flex";
-            content.style.flexDirection = "column";
-            content.style.justifyContent = "center";
-            content.style.height = "100%";
-            content.style.width = "calc(100% - 2rem)";
-            content.style.marginRight = "1rem";
-            content.style.marginLeft = "1rem";
-
-            content.appendChild(gauge);
-            content.appendChild(gaugeValue);
-        } else {
-            const gauge = content.children[0];
-            gaugeBar = gauge.children[0] as HTMLDivElement;
-            gaugeValue = content.children[1] as HTMLSpanElement;
-        }
-
-        // conditional formatting:
-        if (sales < 400000) {
-            gaugeValue.style.color = "rgb(211, 17, 3)";
-            gaugeBar.style.background = "rgb(211, 17, 3)";
-        }
-        else if (sales < 650000) {
-            gaugeValue.style.color = "Orange";
-            gaugeBar.style.background = "Orange";
-        }
-        else {
-            gaugeValue.style.color = "rgb(21, 190, 6)";
-            gaugeBar.style.background = "rgb(21, 190, 6)";
-        }
-        gaugeValue.textContent = "$" + sales / 1000 + ",000";
-
-        let gaugeWidth = (sales / 990000) * 100;
-        gaugeWidth = Math.min(100, gaugeWidth);
-        gaugeBar.style.width = gaugeWidth + "%";
-    }
-
-    public onEmailCellUpdating(s: IgrTemplateColumn, e: IgrTemplateCellUpdatingEventArgs) {
-        const content = e.content as HTMLDivElement;
-        const info = e.cellInfo as IgrTemplateCellInfo;
-        const item = info.rowItem;
-        let link: HTMLAnchorElement;
-
-        if (content.childElementCount === 0) {
-            link = document.createElement("a");
-
-            content.style.verticalAlign = "center";
-            content.style.marginTop = "15px";
-            content.style.lineHeight = "normal";
-            content.style.display = "inline-grid";
-            content.style.fontFamily = "Verdana";
-            content.style.fontSize = "13px";
-            content.style.color = "#4286f4";
-            content.style.width = "100%";
-
-            content.appendChild(link);
-        } else {
-            link = content.children[0] as HTMLAnchorElement;
-        }
-
-        link.href = "mailto:" + item.Email + "?Subject=Hello%20Friend";
-        link.textContent = item.Email;
-    }
-
-    public onPhoneCellUpdating(s: IgrTemplateColumn, e: IgrTemplateCellUpdatingEventArgs) {
-        const content = e.content as HTMLDivElement;
-
-        const info = e.cellInfo as IgrTemplateCellInfo;
-        const item = info.rowItem;
-        let link: HTMLAnchorElement;
-
-        if (content.childElementCount === 0) {
-
-            link = document.createElement("a");
-
-            content.style.verticalAlign = "center";
-            content.style.marginTop = "15px";
-            content.style.lineHeight = "normal";
-            content.style.display = "inline-block";
-            // content.style.display = "inline-grid";
-            content.style.fontFamily = "Verdana";
-            content.style.fontSize = "13px";
-            content.style.color = "#4286f4";
-            content.style.width = "100%";
-
-            content.appendChild(link);
-        } else {
-            link = content.children[0] as HTMLAnchorElement;
-        }
-
-        link.href = "tel:" + item.Phone;
-        link.textContent = item.Phone;
-    }
-
-    public onLoadLayoutClicked() {
-        this.grid.loadLayout(this.savedLayout);
-    }
-
-    public onSaveLayoutClicked() {
-        
-        this.grid.saveLayout();
-        this.savedLayout = this.grid.saveLayout();
-    }
+    
 
 }
 
