561bd99193acea6fa95d972fa7029534ccc8ae15
Michael DiFilippo <mdifilippo@infragistics.com>
2021-12-15 11:30:15 -0500

    Update index.tsx
    Remove white spaces

--- samples/grids/data-grid/column-filter-operands/src/index.tsx
+++ samples/grids/data-grid/column-filter-operands/src/index.tsx
@@ -31,48 +31,48 @@ export default class DataGridColumnFilterOperands extends React.Component<any, a
         if (!grid) { return; }
 
         this.grid = grid;
-        
-        const filterOperand = new IgrFilterOperand();
-        filterOperand.editorType = EditorType.Text;
-        filterOperand.displayName = "Show Only France";
-        filterOperand.filterRequested = this.onFilter;
-        let column = this.grid.actualColumns.item(0);
-        column.filterOperands.add(filterOperand);
-
+        
+        const filterOperand = new IgrFilterOperand();
+        filterOperand.editorType = EditorType.Text;
+        filterOperand.displayName = "Show Only France";
+        filterOperand.filterRequested = this.onFilter;
+        let column = this.grid.actualColumns.item(0);
+        column.filterOperands.add(filterOperand);
+        
         const filterOperand2 = new IgrFilterOperand();
-        filterOperand2.editorType = EditorType.Numeric;
-        filterOperand2.displayName = "Less Than Age 30";
-        filterOperand2.filterRequested = this.onFilter2;
-        let column2 = this.grid.actualColumns.item(1);
-        column2.filterOperands.add(filterOperand2);
-
+        filterOperand2.editorType = EditorType.Numeric;
+        filterOperand2.displayName = "Less Than Age 30";
+        filterOperand2.filterRequested = this.onFilter2;
+        let column2 = this.grid.actualColumns.item(1);
+        column2.filterOperands.add(filterOperand2);
+        
         const filterOperand3 = new IgrFilterOperand();
-        filterOperand3.editorType = EditorType.Numeric;
-        filterOperand3.displayName = "Greater Than $500k";
-        filterOperand3.filterRequested = this.onFilter3;
-        let column3 = this.grid.actualColumns.item(2);
-        column3.filterOperands.add(filterOperand3);
+        filterOperand3.editorType = EditorType.Numeric;
+        filterOperand3.displayName = "Greater Than $500k";
+        filterOperand3.filterRequested = this.onFilter3;
+        let column3 = this.grid.actualColumns.item(2);
+        column3.filterOperands.add(filterOperand3);
     }
 
     public onFilter(s: IgrFilterOperand, args: IgrGridCustomFilterRequestedEventArgs)
     {
         let prop = args.filterFactory.property(args.column.field);
-        //Filter-in only records with France
-        args.expression = prop.isEqualTo("France");
+        //Filter-in only records with France
+        args.expression = prop.isEqualTo("France");
     }
 
     public onFilter2(s: IgrFilterOperand, args: IgrGridCustomFilterRequestedEventArgs)
     {
         let prop = args.filterFactory.property(args.column.field);
-        //Filter-in only records with LessThan or Equal to 30
-        args.expression = prop.isLessThanOrEqualTo(30);
+        //Filter-in only records with LessThan or Equal to 30
+        args.expression = prop.isLessThanOrEqualTo(30);
     }
 
     public onFilter3(s: IgrFilterOperand, args: IgrGridCustomFilterRequestedEventArgs)
     {
         let prop = args.filterFactory.property(args.column.field);
-        //Filter-in only records with GreaterThan $500,000.00
-        args.expression = prop.isGreaterThan(500000);
+        //Filter-in only records with GreaterThan $500,000.00
+        args.expression = prop.isGreaterThan(500000);
     }
 
     public render(): JSX.Element {
