f1e0df5d7564d7617d79640628cd288de8311aca
Michael DiFilippo <mdifilippo@infragistics.com>
2022-03-18 12:21:50 -0400

    Fix build
    fix build

--- samples/charts/data-chart/composite-chart/src/index.tsx
+++ samples/charts/data-chart/composite-chart/src/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import {
   IgrDataChartCoreModule,
   IgrLegendModule,
