37b1cd4df410968d59e8fdba0dcbdc3dc884d195
Michael DiFilippo <mdifilippo@infragistics.com>
2022-07-19 20:23:55 -0400

    Update index.tsx

--- samples/inputs/icon-button/variant/src/index.tsx
+++ samples/inputs/icon-button/variant/src/index.tsx
@@ -8,9 +8,9 @@ IgrIconButtonModule.register();
 
 export default class IconButtonVariant extends React.Component<any, any> {
 
-    public smallIcon: IgrIconButton;
-    public mediumIcon: IgrIconButton;
-    public largeIcon: IgrIconButton;
+    public icon1: IgrIconButton;
+    public icon2: IgrIconButton;
+    public icon3: IgrIconButton;
 
     constructor(props: any) {
         super(props); 
@@ -45,23 +45,23 @@ export default class IconButtonVariant extends React.Component<any, any> {
     
     public smallRef(icon: IgrIconButton) {
         if (!icon) { return; }
-        this.smallIcon = icon;
+        this.icon1 = icon;
         const iconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/></svg>';
-        this.smallIcon.registerIconFromText("thumb-up", iconText, "material");
+        this.icon1.registerIconFromText("thumb-up", iconText, "material");
     }
 
     public mediumRef(icon: IgrIconButton) {
         if (!icon) { return; }
-        this.mediumIcon = icon;
+        this.icon2 = icon;
         const iconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/></svg>';
-        this.mediumIcon.registerIconFromText("thumb-up", iconText, "material");
+        this.icon2.registerIconFromText("thumb-up", iconText, "material");
     }
 
     public largeRef(icon: IgrIconButton) {
         if (!icon) { return; }
-        this.largeIcon = icon;
+        this.icon3 = icon;
         const iconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/></svg>';
-        this.largeIcon.registerIconFromText("thumb-up", iconText, "material");
+        this.icon3.registerIconFromText("thumb-up", iconText, "material");
     }
 }
 
