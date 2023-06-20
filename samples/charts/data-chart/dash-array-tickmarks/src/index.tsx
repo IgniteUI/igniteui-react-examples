65b8df7c41dc83177b8c54e40628547db3ac81dc
tfsbuild <tfsbuild@infragistics.com>
2023-06-20 15:49:22 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.6.20.3

---
+++ samples/charts/data-chart/dash-array-tickmarks/src/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { IgrDataChartCategoryModule, IgrDataChartInteractivityModule } from 'igniteui-react-charts';
+import { IgrDataChart, IgrCategoryXAxis, IgrNumericYAxis, IgrColumnSeries } from 'igniteui-react-charts';
+import { CountryRenewableElectricityItem, CountryRenewableElectricity } from './CountryRenewableElectricity';
+
+const mods: any[] = [
+    IgrDataChartCategoryModule,
+    IgrDataChartInteractivityModule
+];
+mods.forEach((m) => m.register());
+
+export default class Sample extends React.Component<any, any> {
+    private chart: IgrDataChart
+    private chartRef(r: IgrDataChart) {
+        this.chart = r;
+        this.setState({});
+    }
+    private xAxis: IgrCategoryXAxis
+    private yAxis: IgrNumericYAxis
+    private columnSeries1: IgrColumnSeries
+
+    constructor(props: any) {
+        super(props);
+
+        this.chartRef = this.chartRef.bind(this);
+    }
+
+    public render(): JSX.Element {
+        return (
+        <div className="container sample">
+
+            <div className="container fill">
+                <IgrDataChart
+                    ref={this.chartRef}
+                    computedPlotAreaMarginMode="Series">
+                    <IgrCategoryXAxis
+                        name="xAxis"
+                        dataSource={this.countryRenewableElectricity}
+                        label="year"
+                        tickLength="20"
+                        tickStrokeDashArray="["2", "2"]"
+                        tickStroke="darkslategray"
+                        interval="1">
+                    </IgrCategoryXAxis>
+                    <IgrNumericYAxis
+                        name="yAxis"
+                        tickLength="20"
+                        tickStrokeDashArray="["2", "2"]"
+                        tickStroke="darkslategray"
+                        minimumValue="0">
+                    </IgrNumericYAxis>
+                    <IgrColumnSeries
+                        name="columnSeries1"
+                        title="China"
+                        xAxisName="xAxis"
+                        yAxisName="yAxis"
+                        dataSource={this.countryRenewableElectricity}
+                        valueMemberPath="china"
+                        showDefaultTooltip="true">
+                    </IgrColumnSeries>
+                </IgrDataChart>
+            </div>
+        </div>
+        );
+    }
+
+    private _countryRenewableElectricity: CountryRenewableElectricity = null;
+    public get countryRenewableElectricity(): CountryRenewableElectricity {
+        if (this._countryRenewableElectricity == null)
+        {
+            this._countryRenewableElectricity = new CountryRenewableElectricity();
+        }
+        return this._countryRenewableElectricity;
+    }
+
+}
+
+// rendering above component in the React DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Sample/>);
