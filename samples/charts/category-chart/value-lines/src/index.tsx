8b1a6a8a064d0e7873179ff3ba68f63e332a1951
tfsbuild <tfsbuild@infragistics.com>
2023-06-20 19:40:20 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.6.20.2

---
+++ samples/charts/category-chart/value-lines/src/index.tsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { IgrPropertyEditorPanelModule } from 'igniteui-react-layouts';
+import { IgrLegendModule, IgrCategoryChartModule } from 'igniteui-react-charts';
+import { IgrLegend, IgrCategoryChart } from 'igniteui-react-charts';
+import { IgrPropertyEditorPanel, IgrPropertyEditorPropertyDescription } from 'igniteui-react-layouts';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, LegendDescriptionModule, CategoryChartDescriptionModule } from 'igniteui-react-core';
+import { CountryRenewableElectricityItem, CountryRenewableElectricity } from './CountryRenewableElectricity';
+import { IgrPropertyEditorPropertyDescriptionChangedEventArgs } from 'igniteui-react-layouts';
+import { MarkerType, MarkerType_$type } from 'igniteui-react-charts';
+import { EnumUtil } from 'igniteui-react-core';
+
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+import { defineAllComponents } from 'igniteui-webcomponents';
+
+defineAllComponents();
+
+const mods: any[] = [
+    IgrPropertyEditorPanelModule,
+    IgrLegendModule,
+    IgrCategoryChartModule
+];
+mods.forEach((m) => m.register());
+
+export default class Sample extends React.Component<any, any> {
+    private legend: IgrLegend
+    private legendRef(r: IgrLegend) {
+        this.legend = r;
+        this.setState({});
+    }
+    private propertyEditor: IgrPropertyEditorPanel
+    private propertyEditorRef(r: IgrPropertyEditorPanel) {
+        this.propertyEditor = r;
+        this.setState({});
+    }
+    private valueListEditor: IgrPropertyEditorPropertyDescription
+    private chart: IgrCategoryChart
+    private chartRef(r: IgrCategoryChart) {
+        this.chart = r;
+        this.setState({});
+    }
+
+    constructor(props: any) {
+        super(props);
+
+        this.legendRef = this.legendRef.bind(this);
+        this.propertyEditorRef = this.propertyEditorRef.bind(this);
+        this.editorChangeUpdateValueLines = this.editorChangeUpdateValueLines.bind(this);
+        this.chartRef = this.chartRef.bind(this);
+    }
+
+    public render(): JSX.Element {
+        return (
+        <div className="container sample">
+            <div className="options vertical">
+                <IgrPropertyEditorPanel
+                    ref={this.propertyEditorRef}
+                    componentRenderer={this.renderer}
+                    target={this.chart}
+                    descriptionType="CategoryChart"
+                    isHorizontal="true"
+                    isWrappingEnabled="true">
+                    <IgrPropertyEditorPropertyDescription
+                        propertyPath="ValueListHandler"
+                        name="ValueListEditor"
+                        label="Value List"
+                        shouldOverrideDefaultEditor="true"
+                        valueType="EnumValue"
+                        dropDownValues={["Auto", "Average", "GlobalAverage", "GlobalMaximum", "GlobalMinimum", "Maximum", "Minimum"]}
+                        dropDownNames={["Auto", "Average", "GlobalAverage", "GlobalMaximum", "GlobalMinimum", "Maximum", "Minimum"]}
+                        primitiveValue="Auto"
+                        changed={this.editorChangeUpdateValueLines}>
+                    </IgrPropertyEditorPropertyDescription>
+                </IgrPropertyEditorPanel>
+            </div>
+
+            <div className="legend-title">
+                Renewable Electricity Generated
+            </div>
+
+            <div className="legend">
+                <IgrLegend
+                    ref={this.legendRef}
+                    orientation="Horizontal">
+                </IgrLegend>
+            </div>
+
+            <div className="container fill">
+                <IgrCategoryChart
+                    ref={this.chartRef}
+                    dataSource={this.countryRenewableElectricity}
+                    includedProperties={["year", "america", "europe"]}
+                    chartType="Column"
+                    legend={this.legend}
+                    isHorizontalZoomEnabled="false"
+                    isVerticalZoomEnabled="false"
+                    crosshairsDisplayMode="None">
+                </IgrCategoryChart>
+            </div>
+        </div>
+        );
+    }
+
+    private _countryRenewableElectricity: CountryRenewableElectricity = null;
+    public get countryRenewableElectricity(): CountryRenewableElectricity {
+        if (this._countryRenewableElectricity == null)
+        {
+            this._countryRenewableElectricity = new CountryRenewableElectricity();
+        }
+        return this._countryRenewableElectricity;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            LegendDescriptionModule.register(context);
+            CategoryChartDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+    public editorChangeUpdateValueLines(sender: any, args: IgrPropertyEditorPropertyDescriptionChangedEventArgs): void {
+        var item = sender as IgrPropertyEditorPropertyDescription;
+        var chart = this.chart;
+
+        var valueLineType = item.primitiveValue;
+        chart.valueLines = valueLineType;
+    }
+
+}
+
+// rendering above component in the React DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Sample/>);
