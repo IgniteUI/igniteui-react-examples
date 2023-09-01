339b57e099c921d34196bb84e2b495f35ba0ad41
tfsbuild <tfsbuild@infragistics.com>
2023-09-01 22:51:34 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.9.1.1

---
+++ samples/charts/category-chart/custom-selection/src/SelectableData.ts
@@ -0,0 +1,111 @@
+export class SelectableDataItem {
+    public constructor(init: Partial<SelectableDataItem>) {
+        Object.assign(this, init);
+    }
+
+    public category: string;
+    public dataValue: number;
+    public selectedValue: number;
+
+}
+export class SelectableData extends Array<SelectableDataItem> {
+    public constructor() {
+        super();
+        this.push(new SelectableDataItem(
+        {
+            category: `2010`,
+            dataValue: 20,
+            selectedValue: 20
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2011`,
+            dataValue: 40,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2012`,
+            dataValue: 35,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2013`,
+            dataValue: 50,
+            selectedValue: 50
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2014`,
+            dataValue: 45,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2015`,
+            dataValue: 60,
+            selectedValue: 60
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2016`,
+            dataValue: 35,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2017`,
+            dataValue: 40,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2018`,
+            dataValue: 50,
+            selectedValue: 50
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2019`,
+            dataValue: 75,
+            selectedValue: 75
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2020`,
+            dataValue: 65,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2021`,
+            dataValue: 40,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2022`,
+            dataValue: 50,
+            selectedValue: null
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2023`,
+            dataValue: 65,
+            selectedValue: 65
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2024`,
+            dataValue: 70,
+            selectedValue: 70
+        }));
+        this.push(new SelectableDataItem(
+        {
+            category: `2025`,
+            dataValue: 85,
+            selectedValue: null
+        }));
+    }
+}
