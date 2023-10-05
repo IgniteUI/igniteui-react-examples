86bef54e5ac826590721c72b8c933371146b2f83
tfsbuild <tfsbuild@infragistics.com>
2023-10-05 17:42:35 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.10.5.3

--- samples/grids/grid/column-sorting-options/src/index.tsx
+++ samples/grids/grid/column-sorting-options/src/index.tsx
@@ -77,7 +77,7 @@ export default class Sample extends React.Component<any, any> {
 
     public render(): JSX.Element {
         return (
-        <div className="container sample">
+        <div className="container sample ig-typography">
             <div className="options vertical">
                 <IgrPropertyEditorPanel
                     ref={this.propertyEditorRef}
