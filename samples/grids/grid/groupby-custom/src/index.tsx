e48875c7bd72fc85fd6bcfa964460bb95a57ad0f
skrustev <striker18@mail.bg>
2023-10-18 18:25:11 +0300

    Format the intex file to have matching spaces all around.

--- samples/grids/grid/groupby-custom/src/index.tsx
+++ samples/grids/grid/groupby-custom/src/index.tsx
@@ -9,8 +9,8 @@ import { InvoicesWorldData } from "./InvoicesWorldData";
 import "igniteui-react-grids/grids/combined";
 import "igniteui-react-grids/grids/themes/light/bootstrap.css";
 import {
-    IgrBadge,
-    IgrButton,
+  IgrBadge,
+  IgrButton,
   IgrDropdown,
   IgrDropdownItem,
   IgrDropdownModule
@@ -27,149 +27,150 @@ export default function Sample() {
 
   function getParsedDate(date: any) {
     return {
-        day: date.getDay(),
-        month: date.getMonth() + 1,
-        year: date.getFullYear(),
+      day: date.getDay(),
+      month: date.getMonth() + 1,
+      year: date.getFullYear(),
     };
-    }
-    function getWeekOfDate(date:any) {
-        const onejan = new Date(date.getFullYear(), 0, 1);
-        const week = Math.ceil((((date.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7);
-        return week;
-    }
+  }
+  function getWeekOfDate(date: any) {
+    const onejan = new Date(date.getFullYear(), 0, 1);
+    const week = Math.ceil((((date.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7);
+    return week;
+  }
 
-    function setMode(e: any) {
-        setGroupByMode(e.target.textContent);
-    }
+  function setMode(e: any) {
+    setGroupByMode(e.target.textContent);
+  }
 
 
 
-const [groupByMode, setGroupByMode] = useState("Month");
-const groupExpression = [
-    { fieldName: 'OrderDate', dir: SortingDirection.Desc,
-    groupingComparer: (a:any, b: any) => {
-            const dateA = getParsedDate(a);
-            const dateB = getParsedDate(b);
-            if (groupByMode === 'Month') {
-                return dateA.month === dateB.month ? 0 : -1;
-            } else if (groupByMode === "Year") {
-                return dateA.year === dateB.year ? 0 : -1;
-            } else if (groupByMode === 'Week') {
-                return getWeekOfDate(a) === getWeekOfDate(b) ? 0 : -1;
-             }
-            return dateA.day === dateB.day && dateA.month === dateB.month ? 0 : -1;
+  const [groupByMode, setGroupByMode] = useState("Month");
+  const groupExpression = [
+    {
+      fieldName: 'OrderDate', dir: SortingDirection.Desc,
+      groupingComparer: (a: any, b: any) => {
+        const dateA = getParsedDate(a);
+        const dateB = getParsedDate(b);
+        if (groupByMode === 'Month') {
+          return dateA.month === dateB.month ? 0 : -1;
+        } else if (groupByMode === "Year") {
+          return dateA.year === dateB.year ? 0 : -1;
+        } else if (groupByMode === 'Week') {
+          return getWeekOfDate(a) === getWeekOfDate(b) ? 0 : -1;
         }
+        return dateA.day === dateB.day && dateA.month === dateB.month ? 0 : -1;
+      }
     }
-] as any;
+  ] as any;
 
-function getMonthName(date: Date) {
+  function getMonthName(date: Date) {
     const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-   return monthNames[date.getMonth()];
-}
+      "July", "August", "September", "October", "November", "December"
+    ];
+    return monthNames[date.getMonth()];
+  }
 
-function groupByRowTemplate(ctx: { dataContext: IgrGroupByRowTemplateContext}) {
+  function groupByRowTemplate(ctx: { dataContext: IgrGroupByRowTemplateContext }) {
     const groupRow: any = ctx.dataContext.implicit;
 
     const dateTypes = {
-        "Day":  groupRow.value.toLocaleDateString(),
-        "Month": getMonthName(groupRow.value),
-        "Year": groupRow.value.getFullYear(),
-        "Week": getWeekOfDate(groupRow.value)
-      };
+      "Day": groupRow.value.toLocaleDateString(),
+      "Month": getMonthName(groupRow.value),
+      "Year": groupRow.value.getFullYear(),
+      "Week": getWeekOfDate(groupRow.value)
+    };
 
     const value = (dateTypes as any)[groupByMode];
     return <><div>
-        <span style={{color:"#09f"}}>OrderDate:</span>
-        <span>{value}</span>
-        <IgrBadge><span key="badge">{groupRow.records.length}</span></IgrBadge>
-        </div></>;
-}
+      <span style={{ color: "#09f" }}>OrderDate:</span>
+      <span>{value}</span>
+      <IgrBadge><span key="badge">{groupRow.records.length}</span></IgrBadge>
+    </div></>;
+  }
 
   return (
-<div className="container sample ig-typography">
-  <div style={{alignSelf: "flex-end" }} className="fill">
-  <IgrButton key="btn"><span key="content" onClick={x => { dropDownRef.current.toggle()}}>Group By Options</span></IgrButton>
-  <span onClick={e => setMode(e)}>
-  <IgrDropdown ref={dropDownRef}>
-    <IgrDropdownItem key="day"><span key="contentDay">Day</span></IgrDropdownItem>
-    <IgrDropdownItem key="week"><span key="contentWeek">Week</span></IgrDropdownItem>
-    <IgrDropdownItem key="month" selected="true"><span key="contentMonth">Month</span></IgrDropdownItem>
-    <IgrDropdownItem key="year"><span key="contentYear">Year</span></IgrDropdownItem>
-  </IgrDropdown>
-  </span>
-  </div>
-    <div className="container fill">
+    <div className="container sample ig-typography">
+      <div style={{ alignSelf: "flex-end" }} className="fill">
+        <IgrButton key="btn"><span key="content" onClick={x => { dropDownRef.current.toggle() }}>Group By Options</span></IgrButton>
+        <span onClick={e => setMode(e)}>
+          <IgrDropdown ref={dropDownRef}>
+            <IgrDropdownItem key="day"><span key="contentDay">Day</span></IgrDropdownItem>
+            <IgrDropdownItem key="week"><span key="contentWeek">Week</span></IgrDropdownItem>
+            <IgrDropdownItem key="month" selected="true"><span key="contentMonth">Month</span></IgrDropdownItem>
+            <IgrDropdownItem key="year"><span key="contentYear">Year</span></IgrDropdownItem>
+          </IgrDropdown>
+        </span>
+      </div>
+      <div className="container fill">
         <IgrGrid
-        data={data}
-        ref={gridRef}
-        groupRowTemplate={groupByRowTemplate}
-        groupingExpressions={groupExpression}
-        autoGenerate="false">
-            <IgrColumn
+          data={data}
+          ref={gridRef}
+          groupRowTemplate={groupByRowTemplate}
+          groupingExpressions={groupExpression}
+          autoGenerate="false">
+          <IgrColumn
             name="OrderID"
             id="OrderID"
             field="OrderID"
             header="Order ID">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="ShipCountry"
             id="ShipCountry"
             field="ShipCountry"
             header="Ship Country">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="OrderDate"
             id="OrderDate"
             dataType="date"
             field="OrderDate"
             header="Order Date">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="PostalCode"
             id="PostalCode"
             field="PostalCode"
             header="Postal Code">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="Discontinued"
             id="Discontinued"
             field="Discontinued"
             header="Discontinued">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="ShipName"
             id="ShipName"
             field="ShipName"
             header="Ship Name">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="ShipperName"
             id="ShipperName"
             field="ShipperName"
             header="Shipper Name">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="SalesPerson"
             id="SalesPerson"
             field="SalesPerson"
             header="Sales Person">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="UnitPrice"
             id="UnitPrice"
             field="UnitPrice"
             header="Unit Price">
-            </IgrColumn>
-            <IgrColumn
+          </IgrColumn>
+          <IgrColumn
             name="Quantity"
             id="Quantity"
             field="Quantity"
             header="Quantity">
-            </IgrColumn>
+          </IgrColumn>
         </IgrGrid>
-    </div>
+      </div>
     </div>
   );
 }
