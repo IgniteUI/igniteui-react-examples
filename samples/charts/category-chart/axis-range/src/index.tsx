38cd9a53e0ef34c2150a3dfc387ef8a0e0ab8d2b
tfsbuild <tfsbuild@infragistics.com>
2024-01-15 20:44:14 +0200

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.1.15.2

--- samples/charts/category-chart/axis-range/src/index.tsx
+++ samples/charts/category-chart/axis-range/src/index.tsx
@@ -8,6 +8,9 @@ import { IgrLegend, IgrCategoryChart } from 'igniteui-react-charts';
 import { IgrPropertyEditorPanel, IgrPropertyEditorPropertyDescription } from 'igniteui-react-layouts';
 import { ComponentRenderer, PropertyEditorPanelDescriptionModule, LegendDescriptionModule, CategoryChartDescriptionModule } from 'igniteui-react-core';
 import { CountryRenewableElectricityItem, CountryRenewableElectricity } from './CountryRenewableElectricity';
+import { IgrPropertyEditorPropertyDescriptionChangedEventArgs } from 'igniteui-react-layouts';
+import { MarkerType, MarkerType_$type } from 'igniteui-react-charts';
+import { EnumUtil } from 'igniteui-react-core';
 
 import 'igniteui-webcomponents/themes/light/bootstrap.css';
 
@@ -42,6 +45,8 @@ export default class Sample extends React.Component<any, any> {
 
         this.legendRef = this.legendRef.bind(this);
         this.propertyEditorPanel1Ref = this.propertyEditorPanel1Ref.bind(this);
+        this.editorChangeUpdateYAxisMinimumValue = this.editorChangeUpdateYAxisMinimumValue.bind(this);
+        this.editorChangeUpdateYAxisMaximumValue = this.editorChangeUpdateYAxisMaximumValue.bind(this);
         this.chartRef = this.chartRef.bind(this);
     }
 
@@ -57,24 +62,24 @@ export default class Sample extends React.Component<any, any> {
                     isWrappingEnabled="true"
                     ref={this.propertyEditorPanel1Ref}>
                     <IgrPropertyEditorPropertyDescription
-                        propertyPath="YAxisMinimumValue"
+                        propertyPath="YAxisMinimumValueHandler"
                         name="YAxisMinimumValue"
                         label="Y Axis Minimum Value"
                         shouldOverrideDefaultEditor="true"
                         valueType="EnumValue"
                         dropDownNames={["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]}
                         dropDownValues={["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]}
-                        primitiveValue="0">
+                        changed={this.editorChangeUpdateYAxisMinimumValue}>
                     </IgrPropertyEditorPropertyDescription>
                     <IgrPropertyEditorPropertyDescription
-                        propertyPath="YAxisMaximumValue"
+                        propertyPath="YAxisMaximumValueHandler"
                         name="YAxisMaximumValue"
                         label="Y Axis Maximum Value"
                         shouldOverrideDefaultEditor="true"
                         valueType="EnumValue"
                         dropDownNames={["100", "110", "120", "130", "140", "150", "160", "170", "180", "190", "200"]}
                         dropDownValues={["100", "110", "120", "130", "140", "150", "160", "170", "180", "190", "200"]}
-                        primitiveValue="150">
+                        changed={this.editorChangeUpdateYAxisMaximumValue}>
                     </IgrPropertyEditorPropertyDescription>
                 </IgrPropertyEditorPanel>
             </div>
@@ -127,6 +132,18 @@ export default class Sample extends React.Component<any, any> {
             CategoryChartDescriptionModule.register(context);
         }
         return this._componentRenderer;
+    }
+
+    public editorChangeUpdateYAxisMinimumValue(sender: any, args: IgrPropertyEditorPropertyDescriptionChangedEventArgs): void {
+
+    	var yAxisMinimumVal = args.newValue;
+        this.chart.yAxisMinimumValue = parseInt(yAxisMinimumVal);
+    }
+
+    public editorChangeUpdateYAxisMaximumValue(sender: any, args: IgrPropertyEditorPropertyDescriptionChangedEventArgs): void {
+
+        var yAxisMaximumVal = args.newValue;
+        this.chart.yAxisMaximumValue = parseInt(yAxisMaximumVal);
     }
 
 }
