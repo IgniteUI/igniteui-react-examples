111fa552e2ecb807fdcf8effa0c64cacd1bb8c1b
RivaIvanova <ivanova.riva@gmail.com>
2024-01-18 16:49:40 +0200

    feat(combo): add samples

---
+++ samples/inputs/combo/overview/src/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { IgrComboModule, IgrCombo } from "igniteui-react";
+import "igniteui-webcomponents/themes/light/bootstrap.css";
+
+IgrComboModule.register();
+
+interface City {
+  id: string;
+  name: string;
+}
+
+const cities: City[] = [
+  { name: "London", id: "UK01" },
+  { name: "Sofia", id: "BG01" },
+  { name: "New York", id: "NY01" },
+];
+
+export default function ComboOverview() {
+  return (
+    <div className="sample">
+      <IgrCombo
+        valueKey="id"
+        displayKey="name"
+        data={cities}
+        value={["BG01"]}
+      ></IgrCombo>
+    </div>
+  );
+}
+
+// rendering above class to the React DOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ComboOverview />);
