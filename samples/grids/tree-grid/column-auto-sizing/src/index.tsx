68010f24e2a3792c02cd362daf4c5b10c1c29a2a
RivaIvanova <ivanova.riva@gmail.com>
2024-04-10 17:22:36 +0300

    feat(treeGrid): add column resizing samples

---
+++ samples/grids/tree-grid/column-auto-sizing/src/index.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import {
+  IgrColumn,
+  IgrTreeGrid,
+  IgrTreeGridModule,
+} from "igniteui-react-grids";
+
+import "igniteui-react-grids/grids/combined";
+import "igniteui-react-grids/grids/themes/light/bootstrap.css";
+
+import { EMPLOYEES_DATA } from "./data";
+
+IgrTreeGridModule.register();
+
+export default function App() {
+  return (
+    <div className="container sample ig-typography">
+      <div className="container fill">
+        <IgrTreeGrid
+          data={EMPLOYEES_DATA}
+          primaryKey="ID"
+          foreignKey="ParentID"
+          height="500px"
+        >
+          <IgrColumn
+            field="Name"
+            header="Name"
+            dataType="string"
+            width="220px"
+            minWidth="200px"
+            maxWidth="250px"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="Title"
+            header="Title"
+            dataType="string"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="HireDate"
+            header="Hire Date"
+            dataType="date"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="Age"
+            header="Age"
+            dataType="number"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="Address"
+            header="Address"
+            dataType="string"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="City"
+            header="City"
+            dataType="string"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="Country"
+            header="Country"
+            dataType="string"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="Fax"
+            header="Fax"
+            dataType="string"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="PostalCode"
+            header="Postal Code"
+            dataType="string"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+          <IgrColumn
+            field="Phone"
+            header="Phone"
+            dataType="string"
+            width="auto"
+            resizable={true}
+          ></IgrColumn>
+        </IgrTreeGrid>
+      </div>
+    </div>
+  );
+}
+
+// rendering above component in the React DOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
