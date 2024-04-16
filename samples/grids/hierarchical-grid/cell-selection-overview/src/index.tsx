689508c5c28fe51aa78c015428aa15b7eb32a22f
tfsbuild <tfsbuild@infragistics.com>
2024-04-16 13:14:55 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.4.16.2

---
+++ samples/grids/hierarchical-grid/cell-selection-overview/src/index.tsx
@@ -0,0 +1,213 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { IgrPropertyEditorPanelModule } from 'igniteui-react-layouts';
+import { IgrHierarchicalGridModule } from 'igniteui-react-grids';
+import { IgrPropertyEditorPanel, IgrPropertyEditorPropertyDescription } from 'igniteui-react-layouts';
+import { IgrHierarchicalGrid, IgrColumn, IgrRowIsland } from 'igniteui-react-grids';
+import { ComponentRenderer, PropertyEditorPanelDescriptionModule, WebHierarchicalGridDescriptionModule } from 'igniteui-react-core';
+import SingersData from './SingersData.json';
+
+import 'igniteui-react-grids/grids/combined';
+import 'igniteui-react-grids/grids/themes/light/bootstrap.css';
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+
+const mods: any[] = [
+    IgrPropertyEditorPanelModule,
+    IgrHierarchicalGridModule
+];
+mods.forEach((m) => m.register());
+
+export default class Sample extends React.Component<any, any> {
+    private propertyEditor: IgrPropertyEditorPanel
+    private propertyEditorRef(r: IgrPropertyEditorPanel) {
+        this.propertyEditor = r;
+        this.setState({});
+    }
+    private cellSelectionEditor: IgrPropertyEditorPropertyDescription
+    private hGrid: IgrHierarchicalGrid
+    private hGridRef(r: IgrHierarchicalGrid) {
+        this.hGrid = r;
+        this.setState({});
+    }
+
+    constructor(props: any) {
+        super(props);
+
+        this.propertyEditorRef = this.propertyEditorRef.bind(this);
+        this.hGridRef = this.hGridRef.bind(this);
+    }
+
+    public render(): JSX.Element {
+        return (
+        <div className="container sample ig-typography">
+            <div className="options vertical">
+                <IgrPropertyEditorPanel
+                    ref={this.propertyEditorRef}
+                    componentRenderer={this.renderer}
+                    target={this.hGrid}
+                    descriptionType="WebHierarchicalGrid"
+                    isHorizontal="true"
+                    isWrappingEnabled="true">
+                    <IgrPropertyEditorPropertyDescription
+                        propertyPath="CellSelection"
+                        name="CellSelectionEditor"
+                        valueType="EnumValue"
+                        dropDownNames={["None", "Single", "Multiple"]}
+                        dropDownValues={["None", "Single", "Multiple"]}>
+                    </IgrPropertyEditorPropertyDescription>
+                </IgrPropertyEditorPanel>
+            </div>
+
+            <div className="container fill">
+                <IgrHierarchicalGrid
+                    autoGenerate="false"
+                    data={this.singersData}
+                    id="hGrid"
+                    ref={this.hGridRef}
+                    primaryKey="ID">
+                    <IgrColumn
+                        field="Artist"
+                        header="Artist"
+                        dataType="String"
+                        resizable="true">
+                    </IgrColumn>
+                    <IgrColumn
+                        field="Photo"
+                        header="Photo"
+                        dataType="Image"
+                        minWidth="115px"
+                        resizable="true">
+                    </IgrColumn>
+                    <IgrColumn
+                        field="Debut"
+                        header="Debut"
+                        dataType="Number"
+                        minWidth="88px"
+                        maxWidth="230px"
+                        resizable="true">
+                    </IgrColumn>
+                    <IgrColumn
+                        field="GrammyNominations"
+                        header="Grammy Nominations"
+                        dataType="String"
+                        resizable="true">
+                    </IgrColumn>
+                    <IgrColumn
+                        field="GrammyAwards"
+                        header="Grammy Awards"
+                        dataType="String"
+                        resizable="true">
+                    </IgrColumn>
+                    <IgrRowIsland
+                        childDataKey="Albums"
+                        autoGenerate="false">
+                        <IgrColumn
+                            field="Album"
+                            header="Album"
+                            dataType="String"
+                            resizable="true">
+                        </IgrColumn>
+                        <IgrColumn
+                            field="LaunchDate"
+                            header="Launch Date"
+                            dataType="Date"
+                            resizable="true">
+                        </IgrColumn>
+                        <IgrColumn
+                            field="BillboardReview"
+                            header="Billboard Review"
+                            dataType="String"
+                            resizable="true">
+                        </IgrColumn>
+                        <IgrColumn
+                            field="USBillboard200"
+                            header="US Billboard 200"
+                            dataType="String"
+                            resizable="true">
+                        </IgrColumn>
+                        <IgrRowIsland
+                            childDataKey="Songs"
+                            autoGenerate="false">
+                            <IgrColumn
+                                field="Number"
+                                header="No."
+                                dataType="String"
+                                resizable="true">
+                            </IgrColumn>
+                            <IgrColumn
+                                field="Title"
+                                header="Title"
+                                dataType="String"
+                                resizable="true">
+                            </IgrColumn>
+                            <IgrColumn
+                                field="Released"
+                                header="Released"
+                                dataType="String"
+                                resizable="true">
+                            </IgrColumn>
+                            <IgrColumn
+                                field="Genre"
+                                header="Genre"
+                                dataType="String"
+                                resizable="true">
+                            </IgrColumn>
+                        </IgrRowIsland>
+                    </IgrRowIsland>
+                    <IgrRowIsland
+                        childDataKey="Tours"
+                        autoGenerate="false">
+                        <IgrColumn
+                            field="Tour"
+                            header="Tour"
+                            dataType="String"
+                            resizable="true">
+                        </IgrColumn>
+                        <IgrColumn
+                            field="StartedOn"
+                            header="Started on"
+                            dataType="String"
+                            resizable="true">
+                        </IgrColumn>
+                        <IgrColumn
+                            field="Location"
+                            header="Location"
+                            dataType="String"
+                            resizable="true">
+                        </IgrColumn>
+                        <IgrColumn
+                            field="Headliner"
+                            header="Headliner"
+                            dataType="String"
+                            resizable="true">
+                        </IgrColumn>
+                    </IgrRowIsland>
+                </IgrHierarchicalGrid>
+            </div>
+        </div>
+        );
+    }
+
+    private _singersData: any[] = SingersData;
+    public get singersData(): any[] {
+        return this._singersData;
+    }
+
+    private _componentRenderer: ComponentRenderer = null;
+    public get renderer(): ComponentRenderer {
+        if (this._componentRenderer == null) {
+            this._componentRenderer = new ComponentRenderer();
+            var context = this._componentRenderer.context;
+            PropertyEditorPanelDescriptionModule.register(context);
+            WebHierarchicalGridDescriptionModule.register(context);
+        }
+        return this._componentRenderer;
+    }
+
+}
+
+// rendering above component in the React DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Sample/>);
