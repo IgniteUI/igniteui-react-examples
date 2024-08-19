a5d8dfaf25eae8b23d596d10bc1179e0c5dba919
skrustev <striker18@mail.bg>
2024-08-19 17:28:20 +0300

    Update old DatePicker references.

--- samples/editors/date-picker/range/src/index.tsx
+++ samples/editors/date-picker/range/src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "./date-picker-range.css";
-import { IgrDatePicker, IgrDatePickerModule, IgrSelectedValueChangedEventArgs } from 'igniteui-react-inputs';
+import { IgrXDatePicker, IgrXDatePickerModule, IgrSelectedValueChangedEventArgs } from 'igniteui-react-inputs';
 
-IgrDatePickerModule.register();
+IgrXDatePickerModule.register();
 
 export default class DatePickerRange extends React.Component<any, any> {
 
-    public fromDatePickerRef: IgrDatePicker;
-    public toDatePickerRef: IgrDatePicker;
+    public fromDatePickerRef: IgrXDatePicker;
+    public toDatePickerRef: IgrXDatePicker;
 
     constructor(props: any) {
         super(props);
@@ -24,15 +24,15 @@ export default class DatePickerRange extends React.Component<any, any> {
             <div className="container sample">
                 <div className="dateRanges">
                     <label className="caption">From:  </label>
-                    <IgrDatePicker ref={this.onFromDatePickerRef} height="50px" width="220px" selectedValueChanged={this.fromDateChanged}  />
+                    <IgrXDatePicker ref={this.onFromDatePickerRef} height="50px" width="220px" selectedValueChanged={this.fromDateChanged}  />
                     <label className="caption">  To:  </label>
-                    <IgrDatePicker ref={this.onToDatePickerRef} height="50px" width="220px" selectedValueChanged={this.toDateChanged}/>
+                    <IgrXDatePicker ref={this.onToDatePickerRef} height="50px" width="220px" selectedValueChanged={this.toDateChanged}/>
                 </div>
             </div>
         );
     }
 
-    public onFromDatePickerRef(picker: IgrDatePicker) {
+    public onFromDatePickerRef(picker: IgrXDatePicker) {
         this.fromDatePickerRef = picker;
         if (!picker) return;
 
@@ -40,7 +40,7 @@ export default class DatePickerRange extends React.Component<any, any> {
         picker.allowTextInput = false;
     }
 
-    public onToDatePickerRef(picker: IgrDatePicker) {
+    public onToDatePickerRef(picker: IgrXDatePicker) {
         this.toDatePickerRef = picker;
         if (!picker) return;
 
@@ -48,14 +48,14 @@ export default class DatePickerRange extends React.Component<any, any> {
         picker.allowTextInput = false;
     }
 
-    public fromDateChanged(s: IgrDatePicker, e: IgrSelectedValueChangedEventArgs) {
+    public fromDateChanged(s: IgrXDatePicker, e: IgrSelectedValueChangedEventArgs) {
         let newDate = e.newValue;
         if (this.toDatePickerRef != null && newDate > this.toDatePickerRef.value) {
             this.toDatePickerRef.value = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate() + 1);
         }
     }
 
-    public toDateChanged(s: IgrDatePicker, e: IgrSelectedValueChangedEventArgs) {
+    public toDateChanged(s: IgrXDatePicker, e: IgrSelectedValueChangedEventArgs) {
         let newDate = e.newValue;
         if (this.fromDatePickerRef != null && newDate < this.fromDatePickerRef.value) {
             this.fromDatePickerRef.value = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate() - 1);
