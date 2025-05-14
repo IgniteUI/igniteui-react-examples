1b7586162d6d794a72a99097445c0f43b20f17db
Ivan Minchev <IMinchev@infragistics.com>
2025-05-14 18:44:04 +0300

    Update row-drag-base sample for React 19

--- samples/grids/grid/row-drag-base/src/index.tsx
+++ samples/grids/grid/row-drag-base/src/index.tsx
@@ -2,32 +2,28 @@ import React, { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { IgrGridModule, IgrRowDragEndEventArgs } from 'igniteui-react-grids';
+import { IgrRowDragEndEventArgs } from 'igniteui-react-grids';
 import { IgrGrid, IgrColumn } from 'igniteui-react-grids';
 import { CustomersData } from './CustomersData';
 
 import 'igniteui-react-grids/grids/combined';
 import 'igniteui-react-grids/grids/themes/light/bootstrap.css';
 
-const mods: any[] = [
-    IgrGridModule
-];
-mods.forEach((m) => m.register());
-
 export default function App() {
     const data = new CustomersData();
     const rightGridRef = useRef<IgrGrid>(null);
+    const leftGridRef = useRef<IgrGrid>(null);
 
-    function onGridRowDragEnd(grid: IgrGrid, evt: IgrRowDragEndEventArgs): void {
+    function onGridRowDragEnd(evt: IgrRowDragEndEventArgs): void {
         const ghostElement = evt.detail.dragDirective.ghostElement;
         if (ghostElement != null) {
             const dragElementPos = ghostElement.getBoundingClientRect();
-            const gridPosition =  document.getElementById("rightGrid").getElementsByTagName("igc-grid")[0].getBoundingClientRect();
+            const gridPosition =  document.getElementById("rightGrid").getBoundingClientRect();
             
             const withinXBounds = dragElementPos.x >= gridPosition.x && dragElementPos.x <= gridPosition.x + gridPosition.width;
             const withinYBounds = dragElementPos.y >= gridPosition.y && dragElementPos.y <= gridPosition.y + gridPosition.height;
             if (withinXBounds && withinYBounds) {
-                grid.deleteRow(evt.detail.dragData.key);
+                leftGridRef.current.deleteRow(evt.detail.dragData.key);
                 rightGridRef.current.addRow(evt.detail.dragData.data);
             }
         }
@@ -36,7 +32,7 @@ export default function App() {
     return (
         <div className="container sample">      
             <div className="container horizontal wrapper">
-                <IgrGrid data={data} width="40%" primaryKey='ID' autoGenerate={false} rowDraggable={true} rowDragEnd={onGridRowDragEnd}>
+                <IgrGrid ref={leftGridRef} data={data} width="40%" primaryKey='ID' autoGenerate={false} rowDraggable={true} onRowDragEnd={onGridRowDragEnd}>
                     <IgrColumn field="ID" width="100px"></IgrColumn>
                     <IgrColumn field="CompanyName" width="100px"></IgrColumn>
                     <IgrColumn field="ContactName" width="100px"></IgrColumn>
