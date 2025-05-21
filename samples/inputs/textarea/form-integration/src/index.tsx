a1690720feea30afbe43d1ba9f860ceadbbe8726
Mariela Tihova <MTihova@infragistics.com>
2025-05-22 00:25:39 +0300

    Updated TextArea samples for React 19

--- samples/inputs/textarea/form-integration/src/index.tsx
+++ samples/inputs/textarea/form-integration/src/index.tsx
@@ -1,50 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { IgrTextarea, IgrTextareaModule, IgrButton, IgrButtonModule, IgrToast, IgrToastModule } from 'igniteui-react';
+import { IgrTextarea, IgrButton, IgrToast } from 'igniteui-react';
 import 'igniteui-webcomponents/themes/light/bootstrap.css';
 
-IgrTextareaModule.register();
-IgrButtonModule.register();
-IgrToastModule.register();
+export default function TextAreaFormIntegration() {
 
-export default class TextAreaFormIntegration extends React.Component<any, any> {
+    let toastRef: IgrToast;
 
-    public toastRef: IgrToast;
-
-    constructor(props: any) {
-        super(props);     
-        this.onSubmitButtonClicked = this.onSubmitButtonClicked.bind(this);
-        this.onToastRef = this.onToastRef.bind(this);
-    }
-
-    public render(): JSX.Element {
-        return (
-            <div className="sample">               
-                <form id="form" onSubmit={this.onSubmitButtonClicked}>
-                <IgrTextarea rows={3}  name="user_feedback" label="Your review" required>
-                </IgrTextarea>                
-                <div className="controls">
-                    <IgrButton type="submit"><span>Submit review</span></IgrButton>
-                    <IgrButton type="reset" variant="outlined"><span>Reset</span></IgrButton>                    
-                </div>
-                <IgrToast id="submitted" position="top" display-time="1e3" ref={this.onToastRef}><span>Your review was submitted</span></IgrToast>
-                </form>
-            </div>            
-        );
-    }
-
-    public onToastRef(toast: IgrToast){
+    const onToastRef = (toast: IgrToast) => {
         if (!toast) { return; }
-        this.toastRef = toast;
+        toastRef = toast;
     }
 
-    public onSubmitButtonClicked(e:React.FormEvent<HTMLFormElement>) {
-        if(this.toastRef){
+    const onSubmitButtonClicked = (e:React.FormEvent<HTMLFormElement>) => {
+        if(toastRef){
             e.preventDefault();
-            this.toastRef.show();
+            toastRef.show();
         }
     }
+
+    return (
+        <div className="sample">               
+            <form id="form" onSubmit={onSubmitButtonClicked}>
+            <IgrTextarea rows={3}  name="user_feedback" label="Your review" required>
+            </IgrTextarea>                
+            <div className="controls">
+                <IgrButton type="submit"><span>Submit review</span></IgrButton>
+                <IgrButton type="reset" variant="outlined"><span>Reset</span></IgrButton>                    
+            </div>
+            <IgrToast id="submitted" position="top" display-time="1e3" ref={onToastRef}><span>Your review was submitted</span></IgrToast>
+            </form>
+        </div>            
+    );
 }
 
 // rendering above class to the React DOM
