844a9291feccbf5ad9767873056e81f764c1787c
Ivan Minchev <IMinchev@infragistics.com>
2025-05-22 18:18:59 +0300

    Update data-performance-virtualization sample for React 19

--- samples/grids/hierarchical-grid/data-performance-virtualization/src/index.tsx
+++ samples/grids/hierarchical-grid/data-performance-virtualization/src/index.tsx
@@ -2,14 +2,12 @@ import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { IgrGridCreatedEventArgs, IgrHierarchicalGridModule } from "igniteui-react-grids";
+import { IgrGridCreatedEventArgs } from "igniteui-react-grids";
 import { IgrHierarchicalGrid, IgrColumn, IgrRowIsland } from "igniteui-react-grids";
 
 import "igniteui-react-grids/grids/combined";
 import "igniteui-react-grids/grids/themes/light/bootstrap.css";
 import { getData } from "./RemoteService";
-
-IgrHierarchicalGridModule.register();
 
 export default function App() {
   const hierarchicalGrid = useRef<IgrHierarchicalGrid>(null);
