7322b625e1ec6836df483809a0706fcd289d883d
Mariela Tihova <MTihova@infragistics.com>
2025-05-29 15:33:07 +0300

    Avatar samples: update + add styling sample

--- samples/layouts/avatar/styling/src/index.tsx
+++ samples/layouts/avatar/styling/src/index.tsx
@@ -4,10 +4,11 @@ import './index.css';
 import { IgrAvatar, IgrIcon, registerIconFromText } from 'igniteui-react';
 import 'igniteui-webcomponents/themes/light/bootstrap.css';
 
+
 const homeIcon =
         '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/></svg>';
 
-export default function AvatarShape() {
+export default function AvatarStyling() {
 
     useEffect(() => {
         registerIconFromText("home", homeIcon, "material");
@@ -15,13 +16,21 @@ export default function AvatarShape() {
     
     return (
         <div className="container sample">
-            <IgrAvatar shape="rounded">
-                <IgrIcon name="home" collection="material" />
-            </IgrAvatar>
+
+            <IgrAvatar 
+                src="https://static.infragistics.com/xplatform/images/people/men/1.jpg" 
+                alt="A photo of a man." 
+                className='size-large'/>
+            
+            <div className='sizes'>
+                <IgrAvatar initials='L' className='size-large'/>
+                <IgrAvatar initials='M' className='size-medium'/>
+                <IgrAvatar initials='S' className='size-small'/>
+            </div>
         </div>
     );
 }
 
 // rendering above component to the React DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AvatarShape/>);
+root.render(<AvatarStyling/>);
