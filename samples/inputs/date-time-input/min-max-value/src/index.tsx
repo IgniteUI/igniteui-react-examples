3717b5790b8acbd6f8831a42cc0d03fafd973243
mtrela <martin.trela@gmail.com>
2025-06-17 16:46:24 -0400

    Revert "Merge branch 'master' into vnext"
    This reverts commit 73f2ffa1e93c7c07a80a6df794f405020b081b03, reversing
    changes made to 54f4eb27f30310682734fce31605ba8867e0c01a.

--- samples/inputs/date-time-input/min-max-value/src/index.tsx
+++ samples/inputs/date-time-input/min-max-value/src/index.tsx
@@ -8,10 +8,6 @@ const upIconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="2
 const downIconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 10l5 5 5-5z"/></svg>';
 const clearIconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>';
 
-const upIconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 14l5-5 5 5z"/></svg>';
-const downIconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 10l5 5 5-5z"/></svg>';
-const clearIconText = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>';
-
 export default class DateTimeInputMinMaxValue extends React.Component<any, any> {
 
     private dateTimeInput: IgrDateTimeInput;
--- samples/inputs/dropdown/group/src/index.tsx
+++ samples/inputs/dropdown/group/src/index.tsx
@@ -6,8 +6,6 @@ import 'igniteui-webcomponents/themes/light/bootstrap.css';
 
 const placeIconText = "<svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px'><path d='M0 0h24v24H0z' fill='none'/><path d='M12 12c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm6-1.8C18 6.57 15.35 4 12 4s-6 2.57-6 6.2c0 2.34 1.95 5.44 6 9.14 4.05-3.7 6-6.8 6-9.14zM12 2c4.2 0 8 3.22 8 8.2 0 3.32-2.67 7.25-8 11.8-5.33-4.55-8-8.48-8-11.8C4 5.22 7.8 2 12 2z'/></svg>";
 
-const placeIconText = "<svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px'><path d='M0 0h24v24H0z' fill='none'/><path d='M12 12c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm6-1.8C18 6.57 15.35 4 12 4s-6 2.57-6 6.2c0 2.34 1.95 5.44 6 9.14 4.05-3.7 6-6.8 6-9.14zM12 2c4.2 0 8 3.22 8 8.2 0 3.32-2.67 7.25-8 11.8-5.33-4.55-8-8.48-8-11.8C4 5.22 7.8 2 12 2z'/></svg>";
-
 export default class DropDownGroup extends React.Component<any, any> {
 
     public placeIcon: IgrIcon;
