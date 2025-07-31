7d090472323292adeb19509f6c50e14575066d25
Adrian Petrov <adrian.ptrv1@gmail.com>
2025-07-31 11:50:21 +0300

    chore(date-range-picker): format code

--- samples/scheduling/date-range-picker/custom-ranges/src/index.tsx
+++ samples/scheduling/date-range-picker/custom-ranges/src/index.tsx
@@ -1,39 +1,43 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { IgrDateRangePicker } from "igniteui-react";
-import {
-	CustomDateRange,
-	DateRangeDescriptor,
-	DateRangeType,
-} from "igniteui-react";
-import "igniteui-webcomponents/themes/light/bootstrap.css";
+import React, {useEffect} from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { IgrDateRangePicker } from 'igniteui-react';
+import { CustomDateRange } from 'igniteui-react';
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
 
 export default function DrpCustom() {
-	useEffect(() => {
-		let dateRange = document.querySelector(
-			"igc-date-range-picker"
-		) as IgrDateRangePicker;
-		const minDate = new Date(2025, 4, 5);
-		const maxDate = new Date(2025, 4, 15);
-		dateRange.disabledDates = [
-			{
-				type: DateRangeType.Between,
-				dateRange: [minDate, maxDate],
-			},
-		] as DateRangeDescriptor[];
-	}, []);
+  useEffect(() => {
+    const today = new Date();
+    const nextSeven = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 7
+    );
+    const nextWeek: CustomDateRange[] = [
+      {
+        label: "Next 7 days",
+        dateRange: {
+          start: today,
+          end: nextSeven,
+        },
+      },
+    ];
+    const dateRange = document.querySelector("igc-date-range-picker") as IgrDateRangePicker;
+    dateRange.customRanges = nextWeek;
+    dateRange.usePredefinedRanges = true;
+  }, []);
 
-	return (
-		<div className="container sample center">
-			<IgrDateRangePicker
-				mode="dialog"
-				label="Custom Ranges"
-			></IgrDateRangePicker>
-		</div>
-	);
+  return (
+    <div className="container sample center">
+      <IgrDateRangePicker 
+        mode="dialog" 
+        usePredefinedRanges 
+        label="Custom Ranges">
+      </IgrDateRangePicker>
+    </div>
+  );
 }
 
 // rendering above class to the React DOM
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<DrpCustom />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<DrpCustom/>);
--- samples/scheduling/date-range-picker/styling/src/index.tsx
+++ samples/scheduling/date-range-picker/styling/src/index.tsx
@@ -2,33 +2,31 @@ import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import {
-	IgrDateRangePicker,
-	IgrIcon,
-	registerIconFromText,
+  IgrDateRangePicker,
+  IgrIcon,
+  registerIconFromText,
 } from "igniteui-react";
 import "igniteui-webcomponents/themes/light/bootstrap.css";
 
 export default function DrpOverview() {
-	const UpPlane = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-120v-80h720v80H120Zm70-200L40-570l96-26 112 94 140-37-207-276 116-31 299 251 170-46q32-9 60.5 7.5T864-585q9 32-7.5 60.5T808-487L190-320Z"/></svg>';
+  const UpPlane = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-120v-80h720v80H120Zm70-200L40-570l96-26 112 94 140-37-207-276 116-31 299 251 170-46q32-9 60.5 7.5T864-585q9 32-7.5 60.5T808-487L190-320Z"/></svg>';
+  const DownPlane = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-120v-80h720v80H120Zm622-202L120-499v-291l96 27 48 139 138 39-35-343 115 34 128 369 172 49q25 8 41.5 29t16.5 48q0 35-28.5 61.5T742-322Z"/></svg>';
 
-	const DownPlane = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-120v-80h720v80H120Zm622-202L120-499v-291l96 27 48 139 138 39-35-343 115 34 128 369 172 49q25 8 41.5 29t16.5 48q0 35-28.5 61.5T742-322Z"/></svg>';
+  registerIconFromText("UpPlaneIcon", UpPlane);
+  registerIconFromText("DownPlaneIcon", DownPlane);
 
-	registerIconFromText("UpPlaneIcon", UpPlane);
-	registerIconFromText("DownPlaneIcon", DownPlane);
-
-	return (
-		<div className="container sample center">
-			<IgrDateRangePicker
-				labelStart="Departure date"
-				labelEnd="Arrival date"
-				useTwoInputs
-			>
-				<IgrIcon slot="calendar-icon-start" name="UpPlaneIcon"></IgrIcon>
-				<IgrIcon slot="calendar-icon-end" name="DownPlaneIcon"></IgrIcon>
-				<span slot="separator"></span>
-			</IgrDateRangePicker>
-		</div>
-	);
+  return (
+    <div className="container sample center">
+      <IgrDateRangePicker
+        labelStart="Departure date"
+        labelEnd="Arrival date"
+        useTwoInputs>
+        <IgrIcon slot="calendar-icon-start" name="UpPlaneIcon"></IgrIcon>
+        <IgrIcon slot="calendar-icon-end" name="DownPlaneIcon"></IgrIcon>
+        <span slot="separator"></span>
+        </IgrDateRangePicker>
+    </div>
+  );
 }
 
 // rendering above class to the React DOM
