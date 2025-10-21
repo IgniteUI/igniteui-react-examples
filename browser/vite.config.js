646e4d81612e28029654bd91bc9aa21d6bb70de3
igdmdimitrov <dmdimitrov@infragistics.com>
2025-10-21 16:51:51 +0300

    chore(*): fixes for licensed packages and themes imports

--- browser/vite.config.js
+++ browser/vite.config.js
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths()
+  ],
+  resolve: {
+    alias: {
+      'igniteui-react-grids/grids': resolve(__dirname, 'node_modules/igniteui-react-grids/grids'),
+      'igniteui-react-grids/grids/themes/light/': resolve(__dirname, 'node_modules/igniteui-react-grids/grids/themes/light/'),
+      '@infragistics/igniteui-react-grids/grids': resolve(__dirname, 'node_modules/@infragistics/igniteui-react-grids/grids'),
+      '@infragistics/igniteui-react-grids/grids/themes/light/': resolve(__dirname, 'node_modules/@infragistics/igniteui-react-grids/grids/themes/light/')
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'pako',
+      'jszip'
+    ]
+  },
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/node_modules/]
+    }
   },
   server: {
     open: false
