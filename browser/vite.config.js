1a9f3c556e6e8ed8988b65837f47ca4433c8509a
Stamen Stoychev <SStoychev@infragistics.com>
2025-10-22 18:11:39 +0300

    fix(cd): resolving issue with iis deploy

--- browser/vite.config.js
+++ browser/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'production' ? '/react-demos/' : '/',
   build: {
     outDir: 'build'
   },
   server: {
     open: false
   },
-});
+}));
