2b6375c7043abaf0860e3c922b1534c29548a10e
Galina Edinakova <gedinakova@infragistics.com>
2025-10-23 10:26:54 +0300

    fix(Excel): Shimming the jszip export to resolve constructor error.

--- browser/vite.config.js
+++ browser/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
+import path, { resolve } from 'path';
 
+const jszipShimPath = path.resolve(__dirname, 'src/jszip-shim.js');
 export default defineConfig({
   plugins: [
     react(),
@@ -13,14 +14,17 @@ export default defineConfig({
       'igniteui-react-grids/grids': resolve(__dirname, 'node_modules/igniteui-react-grids/grids'),
       'igniteui-react-grids/grids/themes/light/': resolve(__dirname, 'node_modules/igniteui-react-grids/grids/themes/light/'),
       '@infragistics/igniteui-react-grids/grids': resolve(__dirname, 'node_modules/@infragistics/igniteui-react-grids/grids'),
-      '@infragistics/igniteui-react-grids/grids/themes/light/': resolve(__dirname, 'node_modules/@infragistics/igniteui-react-grids/grids/themes/light/')
+      '@infragistics/igniteui-react-grids/grids/themes/light/': resolve(__dirname, 'node_modules/@infragistics/igniteui-react-grids/grids/themes/light/'),
+      find: /^jszip(\/dist\/jszip)?$/, 
+      replacement: jszipShimPath,
     }
   },
   optimizeDeps: {
     include: [
       'pako',
-      'jszip'
-    ]
+      'jszip', 
+      'jszip/dist/jszip'
+    ],
   },
   build: {
     outDir: 'build',
