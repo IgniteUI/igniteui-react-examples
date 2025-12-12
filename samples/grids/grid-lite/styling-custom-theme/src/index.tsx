4f0c175ed0aa876852fe500a5a1046c36d5fe856
didimmova <d.dimova11@gmail.com>
2025-12-12 14:54:24 +0200

    feat(grid-lite): update styling sample to use sass theme

--- samples/grids/grid-lite/styling-custom-theme/src/index.tsx
+++ samples/grids/grid-lite/styling-custom-theme/src/index.tsx
@@ -10,7 +10,7 @@ import {
 } from 'igniteui-webcomponents';
 
 import "igniteui-webcomponents/themes/light/bootstrap.css";
-import "./index.css";
+import "./index.scss";
 
 // Register components
 IgcGridLite.register();
