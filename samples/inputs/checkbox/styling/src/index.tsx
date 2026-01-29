95453fb81f7e62c071f707f9bdbf51c457702b4d
Mariela Tihova <mtihova@infragistics.com>
2026-01-29 09:52:38 +0200

    Add a styling sample for Checkbox (#1018)
    * feat(checkbox): Add a styling sample
    
    * fix(checkbox): add correct colors for styling sample

---
+++ samples/inputs/checkbox/styling/src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { IgrCheckbox } from 'igniteui-react';
+import 'igniteui-webcomponents/themes/light/bootstrap.css';
+
+export default class CheckboxStyling extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);           
+    }
+
+    public render(): JSX.Element {
+        return (
+            <div className="sample">
+                <IgrCheckbox>
+                    <span>Checkbox</span>
+                </IgrCheckbox>
+            </div>
+        );
+    }
+}
+
+// rendering above class to the React DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CheckboxStyling/>);
