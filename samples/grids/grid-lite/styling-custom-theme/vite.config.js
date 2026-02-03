6b6155dfdca92d0790ec637003345cc504395caa
didimmova <d.dimova11@gmail.com>
2026-02-03 13:29:19 +0200

    feat(grid-lite): update styling sample to use sass theme

--- samples/grids/grid-lite/styling-custom-theme/vite.config.js
+++ samples/grids/grid-lite/styling-custom-theme/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: [resolve(__dirname, 'node_modules')]
+      }
+    }
+  },
   build: {
     outDir: 'build'
   },
