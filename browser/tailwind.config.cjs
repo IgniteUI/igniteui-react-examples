f3080ba6ba44ef3d10ab1ffe524e87889d04f7fd
Ivan Minchev <IMinchev@infragistics.com>
2026-08-04 13:17:39 +0300

    feature(avatar): add tailwind styling sample

---
+++ browser/tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "../samples/layouts/avatar/tailwind-styling/src/**/*.{ts,tsx}",
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+};
---
+++ samples/layouts/avatar/tailwind-styling/tailwind.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
+};
