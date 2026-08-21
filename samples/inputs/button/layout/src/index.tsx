e81cc037223a19a71180214013045e481e3b76c2
Hristo Hristov <HHristov@infragistics.com>
2026-08-21 13:04:16 +0300

    fix(button): remove icon buttons content

--- samples/inputs/button/layout/src/index.tsx
+++ samples/inputs/button/layout/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { IgrButton, IgrIcon, registerIconFromText } from 'igniteui-react';
@@ -9,6 +9,8 @@ const addIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
 const variants: any[] = ['contained', 'outlined', 'flat', 'fab'];
 
 export default function ButtonLayout() {
+    const [variant, setVariant] = useState('contained');
+
     useEffect(() => {
         registerIconFromText('add', addIcon, 'material');
     }, []);
@@ -16,27 +18,35 @@ export default function ButtonLayout() {
     return (
         <div className="container sample">
             <div className="button-container">
-                {variants.map((variant) => (
-                    <div className="button-group" key={variant}>
-                        <span className="button-label">{variant}</span>
-                        <div className="button-row">
-                            <div className="button-item">
-                                <span className="button-sublabel">Left</span>
-                                <IgrButton variant={variant}>
-                                    <IgrIcon slot="prefix" name="add" collection="material" />
-                                    Add
-                                </IgrButton>
-                            </div>
-                            <div className="button-item">
-                                <span className="button-sublabel">Right</span>
-                                <IgrButton variant={variant}>
-                                    Add
-                                    <IgrIcon slot="suffix" name="add" collection="material" />
-                                </IgrButton>
-                            </div>
+                <label className="variant-selector">
+                    <span className="button-label">Button variant</span>
+                    <select value={variant} onChange={(event) => setVariant(event.target.value)}>
+                        {variants.map((option) => (
+                            <option value={option} key={option}>
+                                {option.charAt(0).toUpperCase() + option.slice(1)}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <div className="button-group">
+                    <span className="button-label">{variant.charAt(0).toUpperCase() + variant.slice(1)}</span>
+                    <div className="button-row">
+                        <div className="button-item">
+                            <span className="button-sublabel">Left</span>
+                            <IgrButton variant={variant}>
+                                <IgrIcon slot="prefix" name="add" collection="material" />
+                                Add
+                            </IgrButton>
+                        </div>
+                        <div className="button-item">
+                            <span className="button-sublabel">Right</span>
+                            <IgrButton variant={variant}>
+                                Add
+                                <IgrIcon slot="suffix" name="add" collection="material" />
+                            </IgrButton>
                         </div>
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
--- samples/inputs/button/size/src/index.tsx
+++ samples/inputs/button/size/src/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './ButtonSizingStyle.css';
-import { IgrButton, IgrIcon, IgrIconButton, registerIconFromText } from 'igniteui-react';
+import { IgrButton, IgrIcon, registerIconFromText } from 'igniteui-react';
 import 'igniteui-webcomponents/themes/light/material.css';
 
 const notificationsIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/></svg>';
@@ -36,22 +36,6 @@ export default function ButtonSize() {
                     </div>
                 ))}
 
-                {sizes.map((size) => (
-                    <div className="size-row" key={'icon-' + size}>
-                        {variants.map((variant) => (
-                            <IgrIconButton
-                                key={variant}
-                                className={'size-' + size}
-                                variant={variant}
-                                name="add"
-                                collection="material"
-                            />
-                        ))}
-                        <IgrButton className={'size-' + size} variant="fab" aria-label="Add">
-                            <IgrIcon name="add" collection="material" />
-                        </IgrButton>
-                    </div>
-                ))}
             </div>
         </div>
     );
